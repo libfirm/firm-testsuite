cc47fee1f3d687a252747961a4316c2298249dfb
Matthias Braun <matze@braunis.de>
2008-11-01 21:33:33 +0000

    build reference app before test
    [r23376]

--- default_test.sh
+++ default_test.sh
@@ -1,14 +1,5 @@
 . helpers.sh
 
-# build+execute test executable
-exe_test="$BUILDDIR_TEST/$name.exe"
-cmd="${TEST_COMPILER} ${file} ${TEST_CFLAGS} ${CFLAGS} ${FILE_FLAGS} ${LINKFLAGS} -o ${exe_test}"
-execute_cmd "$cmd"
-
-res_test="$BUILDDIR_TEST/${name}_result_test.txt"
-cmd="${exe_test} >& $res_test"
-execute_cmd "$cmd"
-
 # build+execute reference executable
 exe_ref="$BUILDDIR_REF/$name.exe"
 cmd="${REF_COMPILER} ${REF_CFLAGS} ${CFLAGS} ${file} ${LINKFLAGS} -o ${exe_ref}"
@@ -17,6 +8,15 @@ execute_cmd "$cmd" "reference compiler failed"
 res_ref="$BUILDDIR_REF/${name}_result_ref.txt"
 cmd="${exe_ref} >& $res_ref"
 execute_cmd "$cmd" "reference executable failed"
+
+# build+execute test executable
+exe_test="$BUILDDIR_TEST/$name.exe"
+cmd="${TEST_COMPILER} ${file} ${TEST_CFLAGS} ${CFLAGS} ${FILE_FLAGS} ${LINKFLAGS} -o ${exe_test}"
+execute_cmd "$cmd"
+
+res_test="$BUILDDIR_TEST/${name}_result_test.txt"
+cmd="${exe_test} >& $res_test"
+execute_cmd "$cmd"
 
 # compare results
 cmd="diff -U100000 $res_test $res_ref > $OUTPUTDIR/${name}.diff.txt"
