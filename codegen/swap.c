304243b4083f3ee36ca98d882dddc6f4dc5871a1
Christoph Mallon <christoph.mallon@gmx.de>
2008-11-02 15:59:21 +0000

    Improve testcase.
    [r23388]

--- codegen/swap.c
+++ codegen/swap.c
@@ -1,31 +1,45 @@
-unsigned word_swap32(unsigned x)
-{
-	return x << 16 | x >> 16;
-}
-
-unsigned short byte_swap16(unsigned short x)
+unsigned short swap16_8(unsigned short x)
 {
 	return x << 8 | x >> 8;
 }
 
-unsigned byte_swap32(unsigned x)
+unsigned swap32_8(unsigned x)
 {
 	return
 		(x >> 24               |
-		 x >>  8 & 0x0000ff00U |
-		 x <<  8 & 0x00ff0000U |
+		 x >>  8 & 0x0000FF00U |
+		 x <<  8 & 0x00FF0000U |
 		 x << 24);
 }
 
-unsigned long long byte_swap64(unsigned long long x)
+unsigned swap32_16(unsigned x)
+{
+	return x << 16 | x >> 16;
+}
+
+unsigned long long swap64_8(unsigned long long x)
 {
 	return
 		(x >> 56                     |
-		 x >> 40 & 0x0000000000ff00U |
-		 x >> 24 & 0x00000000ff0000U |
-		 x >>  8 & 0x000000ff000000U |
-		 x <<  8 & 0x0000ff00000000U |
-		 x << 24 & 0x00ff0000000000U |
-		 x << 40 & 0xff000000000000U |
+		 x >> 40 & 0x0000000000FF00U |
+		 x >> 24 & 0x00000000FF0000U |
+		 x >>  8 & 0x000000FF000000U |
+		 x <<  8 & 0x0000FF00000000U |
+		 x << 24 & 0x00FF0000000000U |
+		 x << 40 & 0xFF000000000000U |
 		 x << 56);
+}
+
+unsigned long long swap64_16(unsigned long long x)
+{
+	return
+		x >> 48                       |
+		x >> 16 & 0x00000000FFFF0000U |
+		x << 16 & 0x0000FFFF00000000U |
+		x << 48;
+}
+
+unsigned long long swap64_32(unsigned long long x)
+{
+	return x << 32 | x >> 32;
 }
