a26c28b67804f94bb66852dd12e1c7bb5c30636e
Michael Beck <beck@ipd.info.uni-karlsruhe.de>
2008-11-04 10:17:50 +0000

    - make this test independent of undefined features
    [r23426]

--- opt/postinc.c
+++ opt/postinc.c
@@ -1,12 +1,15 @@
 static void postinc_break(int *a, int *b) {
 	// assumes sizeof(int) == sizeof(int*)
-	*a = b+1;
-	*b = a+1;
+	*a = (int)(b+1);
+	*b = (int)(a+1);
 }
 
 int main(void) {
-	int a, b;
-	postinc_break(&a, &b);
-	printf("%d\n", a-b);
+	struct x {
+		int a, b;
+	} x;
+
+	postinc_break(&x.a, &x.b);
+	printf("%d\n", x.a - x.b);
 	return 0;
 }
