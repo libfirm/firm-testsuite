3a2194f5add6b6d41e2f6c5bc2f1dab3b2061c73
Michael Beck <beck@ipd.info.uni-karlsruhe.de>
2008-11-22 01:08:02 +0000

    - add ull -> float test
    [r23899]

--- opt/fehler163.c
+++ opt/fehler163.c
@@ -1,14 +1,23 @@
 #include <stdio.h>
 
-double x = 9223372036854775808;
-double y = 56;
+double x = 9223372036854775808.0;
+double y = 56.0;
+
+double u = 9223372036854775808ULL;
+double v = 56ULL;
 
 unsigned long long conv(double v) {
 	return (unsigned long long)v;
 }
 
+double convI(unsigned long  long v) {
+	return (double)v;
+}
+
 int main(int argc, char *argv[]) {
-//	printf("%llu\n", conv(x));
+	printf("%f\n", convI(u));
+	printf("%f\n", convI(v));
+	printf("%llu\n", conv(x));
 	printf("%llu\n", conv(y));
 	return 0;
 }
