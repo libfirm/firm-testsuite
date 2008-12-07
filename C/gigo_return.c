1d019a2265cf9315a7ea79b942950c851633d9b5
Christoph Mallon <christoph.mallon@gmx.de>
2008-12-07 13:03:43 +0000

    gigo_return.c: Test case for broken dominance calculation/control flow optimisation.
    [r24371]

---
+++ C/gigo_return.c
@@ -0,0 +1,19 @@
+int g;
+
+int f(int x)
+{
+	return 0;
+	if (g) {
+		g = 23;
+	} else {
+		g = 42;
+	}
+	/* This return gets gigo'ed on creation, because its value is Bad.  So the
+	 * block ends up with only a kept PhiM (due to the assignments to 'g' above)
+	 * and nothing else.  It is also not kept, because there is no loop.  Its
+	 * dominance depth is never set to -1, because it cannot be reached by a block
+	 * walk or by kept blocks.  So CFopt never properly processes the block and
+	 * its Bad CF predecessors remain, which makes the Bad-verifier after CFopt
+	 * unhappy. */
+	return x;
+}
