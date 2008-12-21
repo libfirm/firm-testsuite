fc584717d3e6a29cfca325a48504d7b2be81cbb8
Michael Beck <beck@ipd.info.uni-karlsruhe.de>
2008-12-21 14:15:49 +0000

    - little testapp for __builtin_prefetch
    [r24833]

---
+++ backend/prefetch.c
@@ -0,0 +1,20 @@
+/* use -marach=k6-2, athlon, pentium4 to see instructions :-) */
+int main(int argc, char *argv[]) {
+	printf("0 0\n");
+	__builtin_prefetch(argv, 0, 0);
+	printf("0 1\n");
+	__builtin_prefetch(argv, 0, 1);
+	printf("0 2\n");
+	__builtin_prefetch(argv, 0, 2);
+	printf("0 3\n");
+	__builtin_prefetch(argv, 0, 3);
+	printf("1 0\n");
+	__builtin_prefetch(argv, 1, 0);
+	printf("1 1\n");
+	__builtin_prefetch(argv, 1, 1);
+	printf("1 2\n");
+	__builtin_prefetch(argv, 1, 2);
+	printf("1 3\n");
+	__builtin_prefetch(argv, 1, 3);
+	return 0;
+}
