7cbced14301f50bede0618cf050d5b30af29a2d8
Matthias Braun <matze@braunis.de>
2009-03-07 23:10:35 +0000

    another attribute problem
    [r25621]

--- C/gnu99/attributes2.c
+++ C/gnu99/attributes2.c
@@ -1,5 +1,7 @@
 __attribute__((noreturn)) void d0(void), __attribute__((format(printf,1,2))) d1(const char*, ...), d2(void);
 
+void foo(void) __attribute__((__const));
+
 int main(void) {
 	return 0;
 }
