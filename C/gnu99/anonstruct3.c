646b8fd1ab95931f2e8a5174005d312c57dbcbe0
Matthias Braun <matze@braunis.de>
2009-03-08 16:19:27 +0000

    more complicated testcase, triggers obscure miscompile with -O3 but not with -O0
    [r25638]

--- C/gnu99/anonstruct3.c
+++ C/gnu99/anonstruct3.c
@@ -1,19 +1,24 @@
 #include <assert.h>
 
+#define offsetof(type,member) ( (char*) &(((type*)0)->member) - ((char*)0) )
+
 struct bla {
 	union {
 		struct {
 			int a, b;
 		};
 		float k;
+		int l;
 	};
 };
 
 int main(void)
 {
 	assert(sizeof(struct bla) == 2*sizeof(int));
+	assert(offsetof(struct bla, a) == offsetof(struct bla, l));
+	assert(offsetof(struct bla, k) == 0);
 	struct bla b;
 	b.a = 42;
 	b.b = 42;
-	return b.a - b.b;
+	return b.l - b.b;
 }
