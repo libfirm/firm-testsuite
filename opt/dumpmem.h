62209fa427a8630364bd062fcd89f83b61980a79
Matthias Braun <matze@braunis.de>
2009-05-13 11:41:55 +0000

    another test
    [r25936]

--- opt/dumpmem.h
+++ opt/dumpmem.h
@@ -1,12 +1,12 @@
-#include <stdio.h>
+extern int printf(const char *str, ...);
 
-static void dumpMem(void* ptr, size_t size)
+static void dumpMem(void* ptr, unsigned size)
 {
-    size_t i;
+    unsigned i;
     unsigned char* p = ptr;
     printf("\n");
     for(i = 0; i < size; ++i) {
-        printf("%02x", p[i]);
+        printf("%02x", (unsigned) p[i]);
         if((i % 4) == 3) {
             printf("\n");
         }
---
+++ opt/fehler167.c
@@ -0,0 +1,42 @@
+<<<<<<< .mine
+int a(int a, int b)
+{
+	return a | !b;
+}
+
+int main(void)
+{
+	return a(0, 0) == 0;
+}
+=======
+int printf(const char *format, ...);
+
+int p, q, r;
+
+void bla(int *x) {
+	*x = 0;
+}
+
+static int test(int a, int b, int c) __attribute__((noinline));
+
+static int test(int a, int b, int c)
+{
+	int res;
+
+	if (a < b)
+		res = 1;
+	else
+		res = 2;
+	if (b < c)
+		res += 1;
+
+	bla(&c);
+	return res;
+}
+
+int main() {
+	int t = test(1, 2, 3) - 2;
+	printf("%d\n", t);
+	return t;
+}
+>>>>>>> .r25935
