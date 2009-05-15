586b7e1277aa8c39975788077ad27dd5532fe7f0
Matthias Braun <matze@braunis.de>
2009-05-15 11:42:44 +0000

    improve testcase
    [r25971]

--- C/gnu99/builtin_expect.c
+++ C/gnu99/builtin_expect.c
@@ -9,8 +9,11 @@ int main(int argc, char **argv)
 	if (__builtin_expect(argc < -10, l)) {
 		return 42;
 	}
-	/* even stranger and apparently still legal... */
-	void *v = __builtin_expect;
+	if(0) {
+		/* even stranger and apparently still legal as long as it is not
+		 * executed ... */
+		void *v = __builtin_expect;
+	}
 
 	return 0;
 }
