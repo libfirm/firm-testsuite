cbe7a55441075f94415ea85179e011d1e08f9358
Matthias Braun <matze@braunis.de>
2009-05-29 16:16:19 +0000

    don't checkin conflicts
    [r26071]

--- opt/fehler169.c
+++ opt/fehler169.c
@@ -1,4 +1,3 @@
-<<<<<<< .mine
 /* strength reduction gets confused with modes */
 unsigned int out;
 
@@ -18,35 +17,3 @@ int main(void)
 	printf("Res: %d (should be 5)\n", out);
 	return 0;
 }
-
-=======
-/*$ -fno-inline */
-int printf(const char *format, ...);
-
-/* variation of fehler166, this time WITHOUT a prototype */
-static int test2(), test();
-
-int main() {
-	/* call BEFORE definition uses the prototype */
-	int t = test(1, 2, 3) - 6;
-	test2();
-	printf("%d\n", t);
-	return t;
-}
-
-static int test(a, b, c)
-	int a;
-	int b;
-	int c;
-{
-	return a+b+c;
-}
-
-int test2(void) {
-	/* call BEFORE definition uses the prototype */
-	int t = test(1, 2, 3) - 6;
-	printf("%d\n", t);
-	return t;
-}
-
->>>>>>> .r26062
