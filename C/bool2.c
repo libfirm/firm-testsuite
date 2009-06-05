31307cf17897882f86111a32f108232dc9dc1455
Matthias Braun <matze@braunis.de>
2009-06-05 13:47:01 +0000

    more advanced bool2 test, move float_ret.c to backend and rename so you don't debug around on athlons just to realize sse is missing
    [r26102]

--- C/bool2.c
+++ C/bool2.c
@@ -3,7 +3,10 @@ extern int printf(const char *str, ...);
 int main(void)
 {
 	_Bool a = 1;
-	a += 1;
+	a += 5;
+	_Bool b = 1;
+	b += (unsigned char) 255;
 	printf("Res: %d (should be 1)\n", (int) a);
+	printf("Res2: %d (should be 1)\n", (int) b);
 	return 0;
 }
