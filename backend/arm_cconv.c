0b754b90e92129900945723117fe4302f69c382e
Matthias Braun <matze@braunis.de>
2009-11-25 17:40:04 +0000

    new tests for float address mode; improve arm cconv test
    [r26743]

---
+++ backend/arm_cconv.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * On arm this function should get parameters passed in 4 register and 1
+ * stackslot:
+ *  r0+r1: double a
+ *  r2: int b
+ *  r3+stackslot: double c
+ */
+double __attribute__((noinline)) func(double a, int b, double c)
+{
+	return a + b + c;
+}
+
+int main(void)
+{
+	double res = func(1.0, 1, 1.0);
+	printf("Result: %f\n", res);
+	return 0;
+}
