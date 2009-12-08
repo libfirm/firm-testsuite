eb50a0980126aa1e35f256decb1152869174a0f3
Matthias Braun <matze@braunis.de>
2009-12-08 23:05:03 +0000

    some comments
    [r26775]

--- C/utf8input.c
+++ C/utf8input.c
@@ -5,7 +5,7 @@
 int main(void)
 {
 #ifdef __STDC_ISO_10646__
-	/* K - normal ascii character (1 byte UTF-8 encoding) */
+	/* K - normal ascii character (a 1 byte UTF-8 encoding) */
 	assert(L"K"[0] == 0x004B);
 	/* "a - german accents (a 2 byte UTF-8 encoding) */
 	assert(L"ä"[0] == 0x00E4);
@@ -13,8 +13,9 @@ int main(void)
 	assert(L"®"[0] == 0x00AE);
 	/* Euro symbol (a 3 byte UTF-8 encoding) */
 	assert(L"€"[0] == 0x20AC);
-	/* violin clef (a 4 bytes UTF-8 encoding) */
+	/* violin clef (a 4 byte UTF-8 encoding) */
 	assert(L"𝄞"[0] == 0x1D11E);
+	/* note: 5 or more byte encodings are forbidden in newer UTF-8 standards */
 #endif
 
 	return 0;
