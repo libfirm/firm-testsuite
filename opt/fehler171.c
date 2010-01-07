4d256fbde0f30b5e7efebf5936c179d674ec9c65
Matthias Braun <matze@braunis.de>
2010-01-07 18:36:02 +0000

    Love testcase
    [r26920]

---
+++ opt/fehler171.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int x = -236478488;
+int y = 1773546;
+
+int main(int argc, char* argv[])
+{
+	long long          a = (long long)x;
+	long long          b = (long long)y;
+	unsigned long long c = (unsigned long long)a;
+	unsigned long long d = (unsigned long long)b;
+
+	printf("Signed:   a / b = %i\n",  (int)(a / b));
+	printf("Unsigned: a / b = %i\n",  (int)(c / d));
+
+	return 0;
+}
