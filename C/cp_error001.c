ac17ca4f0851ff4649d088b339b72d0cb8d09aa4
Sebastian Buchwald <Sebastian.Buchwald@kit.edu>
2010-01-08 16:09:29 +0000

    fehler173 was a Makefile problem
    [r26928]

--- C/cp_error001.c
+++ C/cp_error001.c
@@ -7,5 +7,10 @@ void bi_windup() {
 
 int main(void)
 {
+	int j=0;
+	int i;
+	for(i=0; i<10; i++)
+		j = j++;
+
 	return 0;
 }
--- opt/fehler040.c
+++ opt/fehler040.c
@@ -9,15 +9,13 @@ int main()
 {
 	Y = One;
 	Z = 0.00000000000000011102;
-	printf("Y: %20.20f Z: %20.20f\n", Y, Z);
+	//printf("Y: %20.20f Z: %20.20f\n", Y, Z);
 	/* ... D is power of 1/Radix < 1. */
 	do  {
-		C = Y;
-		Y = Z;
 		Z = Y * Y;
 		//printf("Y: %30.30f Z:%30.30f\n", Y, Z);
 	} while ((Y > Z) && (Z + Z > Z));
 
-	printf("Res. %20.20f - %20.20f\n", Z, Y);
+	//printf("Res. %20.20f - %20.20f\n", Z, Y);
 	return 0;
 }
