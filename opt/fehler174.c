cf595b03fa9355616bbf90c9a5498122531ff170
Matthias Braun <matze@braunis.de>
2010-02-01 17:35:13 +0000

    simplify fehler174.c
    [r27017]

--- opt/fehler174.c
+++ opt/fehler174.c
@@ -1,4 +1,3 @@
-/*$ -O0 $*/
 #include <stdio.h>
 #include <math.h>
 
@@ -9,8 +8,8 @@ int main(void)
 {
 	float pinf = inf;
 	float ninf = minus_inf;
-	int v0 = pinf >= ninf;
-	int v1 = pinf <= pinf;
+	int v0 = pinf >= ninf ? 42 : 13;
+	int v1 = pinf <= pinf ? 42 : 13;
 	/* float simulator messes up stack for the compare sequence */
 	printf("%f >= %f: %d\n", pinf, ninf, v0);
 	printf("%f <= %f: %d\n", pinf, pinf, v1);
