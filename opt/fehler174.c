36737e363225be1e4b1d6e3f8dd7b76b6c1612f0
Matthias Braun <matze@braunis.de>
2010-02-01 17:33:27 +0000

    another love bug
    [r27016]

---
+++ opt/fehler174.c
@@ -0,0 +1,19 @@
+/*$ -O0 $*/
+#include <stdio.h>
+#include <math.h>
+
+float minus_inf = -INFINITY;
+float inf = INFINITY;
+
+int main(void)
+{
+	float pinf = inf;
+	float ninf = minus_inf;
+	int v0 = pinf >= ninf;
+	int v1 = pinf <= pinf;
+	/* float simulator messes up stack for the compare sequence */
+	printf("%f >= %f: %d\n", pinf, ninf, v0);
+	printf("%f <= %f: %d\n", pinf, pinf, v1);
+
+	return 0;
+}
