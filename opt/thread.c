3f03946b854ea01bd182a7668d116b5a21edd539
Matthias Braun <matze@braunis.de>
2010-02-12 17:50:01 +0000

    further extend thread test
    [r27135]

--- opt/thread.c
+++ opt/thread.c
@@ -2,6 +2,8 @@
 #define __thread   __declspec( thread )
 #endif
 
+extern int printf(const char *str, ...);
+
 __thread int tls_g;
 __thread const int h = 42;
 __thread int tls_i[10];
@@ -14,9 +16,13 @@ int test(int i) {
 
 int main()
 {
+	static __thread int tl1;
+	static __thread int tl2 = 42;
     printf("tls_i: %d\n", test(3));
     printf("tls_j: %d\n", tls_j);
     printf("tls_d: %d\n", tls_g);
     printf("tls_cnst: %d\n", h);
+    printf("tls_cnst: %d\n", tl1);
+    printf("tls_cnst: %d\n", tl2);
     return 0;
 }
