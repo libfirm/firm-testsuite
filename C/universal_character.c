27eaabfde77638d89d19171d4f0510cfb940b4a8
Matthias Braun <matze@braunis.de>
2010-02-24 10:00:23 +0000

    'improve' universal_character.c so even icc breaks ;-)
    [r27207]

--- C/universal_character.c
+++ C/universal_character.c
@@ -1,10 +1,17 @@
 int \U00002115 = 42;
 int \u9FA5 = 21;
+int n\u9FA5 = 100;
+
+/* some compilers (=icc) mangle utf8 idents with _uXXXX which might already
+ * be used by existing literals */
+int n_u9fa5 = 7;
+int n_u9FA5 = 13;
 
 extern int printf(const char *str, ...);
 
 int main(void)
 {
 	printf("Dollar: \u0024\n");
-	return \u2115 - \U00009FA5*2;
+	printf("Some Numbers: %d %d %d\n", n\U00009fa5, n_u9FA5, n_u9fa5);
+	return \u2115 - \U00009Fa5*2;
 }
