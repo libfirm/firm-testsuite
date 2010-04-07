64b1fdfc20ce903883beb573abb2718ccccc1db2
Matthias Braun <matze@braunis.de>
2010-04-07 16:42:47 +0000

    extend external firm test to do a full bootstrap of libfirm+cparser
    [r27361]

--- external/firm.sh
+++ external/firm.sh
@@ -6,10 +6,18 @@
 
 FIRMVERSION="1.17.0"
 grab "http://switch.dl.sourceforge.net/sourceforge/libfirm/libfirm-$FIRMVERSION.tar.bz2"
+CPARSERVERSION="0.9.9"
+grab "http://switch.dl.sourceforge.net/sourceforge/cparser/cparser-$CPARSERVERSION.tar.bz2"
 pushd $BUILDDIR > /dev/null
 echo "Extracting..."
 tar -xf "$PACKAGEDIR/libfirm-$FIRMVERSION.tar.bz2"
+tar -xf "$PACKAGEDIR/cparser-$CPARSERVERSION.tar.bz2"
 cd "libfirm-$FIRMVERSION"
 CC=cparser ./configure --prefix=/tmp/firmtest
 make $MAKEFLAGS
+make install
+cd "../cparser-$CPARSERVERSION"
+export PKG_CONFIG_PATH="/tmp/firmtest/lib/pkgconfig"
+export LD_LIBRARY_PATH="/tmp/firmtest/lib:$LD_LIBRARY_PATH"
+make $MAKEFLAGS bootstrap2
 popd > /dev/null
