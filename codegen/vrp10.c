9b131b0cf887d357f427ac02d72efd079a2f2e30
Christoph Mallon <christoph.mallon@gmx.de>
2010-04-22 16:17:33 +0000

    Repeat after me: + has higher precedence than <<.
    [r27423]

--- codegen/vrp10.c
+++ codegen/vrp10.c
@@ -7,8 +7,8 @@ void f(unsigned const* const p1, unsigned char const p2)
 	unsigned char cl  = 10;
 	eax = eax & 0xFFFF0000 | (((eax & 0x0000FFFF) % cl) & 0xFF) << 8 | (eax & 0x0000FFFF) / cl & 0xFF;
 	eax = eax & 0xFFFF00FF | p2 << 8 & 0x0000FF00;
-	eax = eax & 0xFFFFFF00 | (eax + '0')      & 0x000000FF;
-	eax = eax & 0xFFFF00FF | (eax + '0' << 8) & 0x0000FF00;
+	eax = eax & 0xFFFFFF00 | (eax + '0')        & 0x000000FF;
+	eax = eax & 0xFFFF00FF | (eax + ('0' << 8)) & 0x0000FF00;
 	b = eax;
 	eax = eax & 0xFFFFFF00 | (eax - '0') & 0x000000FF;
 	eax = eax & 0xFFFF0000 | (eax & 0x000000FF) * cl;
