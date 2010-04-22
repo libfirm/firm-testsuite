dc540e4b68a30f943c1c93b384e023cafa77771d
Matthias Braun <matze@braunis.de>
2010-04-22 17:40:04 +0000

    fix shoulf_fail testscript
    [r27434]

--- C/should_fail/test.sh
+++ C/should_fail/test.sh
@@ -1,9 +1,10 @@
 do_test() {
     trap true ABRT
     BASEDIR="`dirname ${file}`"
+	name="`basename $file .c`"
     dirprefix=`echo "${BASEDIR}" | sed -e "s/\\//_/"`
 
-    exe_test="$BUILDDIR_TEST/${dirprefix}_$name.exe"
+    exe_test="$BUILDDIR/${dirprefix}_$name.exe"
     cmd="${TEST_COMPILER} ${file} ${TEST_CFLAGS} ${CFLAGS} ${FILE_FLAGS} ${LINKFLAGS} -o ${exe_test}"
     #we should fail when the compilation went ok
     echo "Command: $CMD" >> "$logfile"
@@ -13,9 +14,8 @@ do_test() {
     if test "$RES" = "0" -o "$RES" -gt 128; then
     	if test "$RES" -gt 128; then
     		ERROR="aborted"
-            COMPILE_RES="aborted"
         else
-            COMPILE_RES="failed"
+            ERROR="failed"
     	fi
     	return 1
     fi
