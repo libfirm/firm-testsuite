c610909bc646b70f8616c203a563c0d5a4495384
Michael Beck <beck@ipd.info.uni-karlsruhe.de>
2010-07-23 22:20:53 +0000

    add some #ifdefs for MS mode
    [r27795]

--- x86code/fabs.c
+++ x86code/fabs.c
@@ -1,18 +1,33 @@
-static float __attribute__((noinline)) fabs1(float a)
+#include <math.h>
+#include <stdio.h>
+
+#ifdef _MSC_VER
+#define NOINLINE __declspec(noinline)
+
+static double ZERO     = 0.0;
+static double NAN      = 0.0;
+static double INFINITY = 1.0;
+
+#else
+#define NOINLINE __attribute__((noinline))
+#endif
+
+static float NOINLINE fabs1(float a)
 {
 	return a < 0 ? -a : a;
 }
 
-static float __attribute__((noinline)) fabs2(float a)
+static float NOINLINE fabs2(float a)
 {
 	return !(a < 0) ? a : -a;
 }
 
-#include <math.h>
-#include <stdio.h>
-
 int main(void)
 {
+#ifdef _MSC_VER
+	NAN /= ZERO;
+	INFINITY /= ZERO;
+#endif
 	printf("abs1(%f) = %f\n", NAN, fabs1(NAN));
 	printf("abs2(%f) = %f\n", NAN, fabs2(NAN));
 	printf("abs1(%f) = %f\n", 1.0, fabs1(1.0));
