d66cc95f95ef28c470bb2c3a799a9c03ed7d76f7
Matthias Braun <matze@braunis.de>
2010-09-07 11:01:34 +0000

    improve switch tests
    [r27978]

--- opt/switch.c
+++ opt/switch.c
@@ -1,7 +1,8 @@
 #include <assert.h>
 #include <limits.h>
 
-static int switch1(int k) {
+static int switch1(int k)
+{
 	switch(k) {
 	case 42:
 		return 5;
@@ -16,6 +17,11 @@ static int switch1(int k) {
 	return 3;
 }
 
+static int __attribute__((noinline)) switch2(int k)
+{
+	return switch1(k);
+}
+
 int main(void)
 {
 	assert(switch1(42) == 5);
@@ -25,5 +31,12 @@ int main(void)
 	assert(switch1(-32000) == 3);
 	assert(switch1(INT_MAX) == 8);
 	assert(switch1(INT_MIN) == 9);
+	assert(switch2(42) == 5);
+	assert(switch2(13) == 7);
+	assert(switch2(-1) == 9);
+	assert(switch2(700) == 3);
+	assert(switch2(-32000) == 3);
+	assert(switch2(INT_MAX) == 8);
+	assert(switch2(INT_MIN) == 9);
 	return 0;
 }
