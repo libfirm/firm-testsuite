7a77a6fbe85f31a5aa7dae08b0688bfb03d69c13
Julius Neuffer <julius.neuffer@student.kit.edu>
2011-02-03 13:36:00 +0000

    segmentation fault in combo.c:3342
    [r28288]

---
+++ opt/fehler179.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+
+void prob(int *a, int *b, int x)
+{
+	int *tripnull = NULL;
+	if (x > 0) {
+		a[x] = tripnull[x];
+		b[x] = tripnull[x];
+	}
+}
+
+int main()
+{
+	return 0;
+}
