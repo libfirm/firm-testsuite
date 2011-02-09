5f8056fb029a5771d9d708403c26636106697128
Matthias Braun <matze@braunis.de>
2011-02-09 14:50:23 +0000

    improve testcase
    [r28356]

--- C/cp_error065.c
+++ C/cp_error065.c
@@ -3,7 +3,12 @@ int foo(int x) {
 	return cnt <= 0 ? 0 : (cnt+1);
 }
 
+int foo2(int x) {
+	int cnt = 0;
+	return cnt > 0 ? (cnt+1) : 0;
+}
+
 int main(void)
 {
-	return foo(0);
+	return foo(0) + foo2(0);
 }
