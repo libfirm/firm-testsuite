15c22144d1183d2bba9172496796e8c01494fdc8
Andreas Zwinkau <zwinkau@kit.edu>
2011-02-17 10:04:57 +0000

    New test case
    Triggers assert in the instruction selection, while gcc compiles.
    
    [r28389]

---
+++ opt/gzip_error.c
@@ -0,0 +1,11 @@
+/* extracted from SPEC 2000 gzip unpack.c */
+
+static int max_len = 0;
+static int leaves [25];
+
+int main(void) {
+   int len = 0;
+   for (;len < max_len;len++) { }
+   leaves[max_len]++;
+   return 0;
+}
