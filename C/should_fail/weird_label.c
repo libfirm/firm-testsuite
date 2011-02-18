5870a4a8889a2a90439cff4e8dfe29958f546632
Christoph Mallon <christoph.mallon@gmx.de>
2011-02-18 17:11:30 +0000

    Fix typo in testcase, which causes unnecessary warning.
    [r28415]

--- C/should_fail/weird_label.c
+++ C/should_fail/weird_label.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int main(int argc, char *argv) {
+int main(int argc, char** argv) {
 	switch (argc) {
 		({ weird: printf("It's forbidden to jump here\n"); argc; });
 	case -1:
