bc148e63b2703b11e8361197a730604c95aba222
Andreas Zwinkau <zwinkau@kit.edu>
2011-02-18 13:06:34 +0000

    Simplify testcase
    Sub(Is,P) seems to be right in this case.
    
    [r28408]

--- opt/git_error.c
+++ opt/git_error.c
@@ -1,17 +1,16 @@
 /* source: git 1.7.4.1 abspath.c */
+#include <stdio.h>
 
 int main(void) {
-   char bufs[2][2];
-   char *buf;
-   char *next_buf;
+   char bufs[2][1] = {{2},{4}};
+   char *next_buf = bufs[0];
    int buf_index = 1;
-   int depth = 1;
-   void* st;
+   int depth = 4;
 
    while (depth--) {
-      int len = readlink(buf, next_buf, 4096);
       buf_index = 1 - buf_index;
       next_buf = bufs[buf_index];
    }
+   printf("%d\n", next_buf[0]);
    return 0;
 }
