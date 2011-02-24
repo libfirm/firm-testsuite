04f650f5cb2e75329c41c9ca6713e2ff3c1da665
Matthias Braun <matze@braunis.de>
2011-02-24 22:08:23 +0000

    more bittest checks
    [r28456]

--- opt/bittest.c
+++ opt/bittest.c
@@ -22,11 +22,42 @@ unsigned __attribute__((noinline)) bit_clear2(unsigned val, unsigned bit)
 	return ((1u << bit) & val) != (1u << bit);
 }
 
+unsigned __attribute__((noinline)) bit_set3(unsigned val, unsigned bit)
+{
+	/* this should be normalized to the same as bit_set by firm */
+	return ((1u << bit) & val) > 0;
+}
+
+unsigned __attribute__((noinline)) bit_clear3(unsigned val, unsigned bit)
+{
+	/* this should be normalized to the same as bit_clear by firm */
+	return ((1u << bit) & val) < (1u << bit);
+}
+
+unsigned __attribute__((noinline)) not_a_bit_clear(unsigned val, unsigned bit)
+{
+	/* this could be normalized to a return 0 (but I haven't seen any compiler
+	 * doing this) */
+	return ((1u << bit) & val) > (1u << bit);
+}
+
 int main(void)
 {
 	assert(bit_set(0xdeadbeef, 18));
 	assert(bit_set2(0xdeadbeef, 18));
+	assert(bit_set3(0xdeadbeef, 18));
+	assert(!bit_set(0xdeadbeef, 17));
+	assert(!bit_set2(0xdeadbeef, 17));
+	assert(!bit_set3(0xdeadbeef, 17));
+
 	assert(bit_clear(0xdeadbeef, 17));
 	assert(bit_clear2(0xdeadbeef, 17));
+	assert(bit_clear3(0xdeadbeef, 17));
+	assert(!bit_clear(0xdeadbeef, 18));
+	assert(!bit_clear2(0xdeadbeef, 18));
+	assert(!bit_clear3(0xdeadbeef, 18));
+
+	assert(!not_a_bit_clear(0xdeadbeef, 18));
+	assert(!not_a_bit_clear(0xdeadbeef, 17));
 	return 0;
 }
--- x86code/bittest.c
+++ x86code/bittest.c
@@ -1,5 +1,5 @@
 /*$ -march=core2 $*/
-/* The above flag should tell the backedn to use the bt instruction */
+/* core2 should use the bt instruction */
 #include <assert.h>
 
 unsigned __attribute__((noinline)) bit_set(unsigned val, unsigned bit)
@@ -24,11 +24,42 @@ unsigned __attribute__((noinline)) bit_clear2(unsigned val, unsigned bit)
 	return ((1u << bit) & val) != (1u << bit);
 }
 
+unsigned __attribute__((noinline)) bit_set3(unsigned val, unsigned bit)
+{
+	/* this should be normalized to the same as bit_set by firm */
+	return ((1u << bit) & val) > 0;
+}
+
+unsigned __attribute__((noinline)) bit_clear3(unsigned val, unsigned bit)
+{
+	/* this should be normalized to the same as bit_clear by firm */
+	return ((1u << bit) & val) < (1u << bit);
+}
+
+unsigned __attribute__((noinline)) not_a_bit_clear(unsigned val, unsigned bit)
+{
+	/* this could be normalized to a return 0 (but I haven't seen any compiler
+	 * doing this) */
+	return ((1u << bit) & val) > (1u << bit);
+}
+
 int main(void)
 {
 	assert(bit_set(0xdeadbeef, 18));
 	assert(bit_set2(0xdeadbeef, 18));
+	assert(bit_set3(0xdeadbeef, 18));
+	assert(!bit_set(0xdeadbeef, 17));
+	assert(!bit_set2(0xdeadbeef, 17));
+	assert(!bit_set3(0xdeadbeef, 17));
+
 	assert(bit_clear(0xdeadbeef, 17));
 	assert(bit_clear2(0xdeadbeef, 17));
+	assert(bit_clear3(0xdeadbeef, 17));
+	assert(!bit_clear(0xdeadbeef, 18));
+	assert(!bit_clear2(0xdeadbeef, 18));
+	assert(!bit_clear3(0xdeadbeef, 18));
+
+	assert(!not_a_bit_clear(0xdeadbeef, 18));
+	assert(!not_a_bit_clear(0xdeadbeef, 17));
 	return 0;
 }
