30ca723efbc7560e4cc0afa19b3b69e7b8ce378d
Julius Neuffer <julius.neuffer@student.kit.edu>
2011-03-25 15:25:49 +0100

    failing assertion in irverify.c when cparser is invoked with -O2

---
+++ opt/fehler183.c
@@ -0,0 +1,20 @@
+struct _IO_FILE;
+typedef struct _IO_FILE FILE;
+
+static int sgf_column = 0;
+
+static void sgf_putc(int c, FILE *file)
+{
+	if (c == '\n')
+		sgf_column = 0;
+	else
+		sgf_column++;
+
+	if (sgf_column > 60);
+}
+
+int main(void)
+{
+	FILE *file = 0;
+	sgf_putc('(', file);
+}
