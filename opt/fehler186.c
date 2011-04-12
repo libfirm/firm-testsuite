d6ce6c82978af98c7176764a0dd0f8ecba70dc5b
Matthias Braun <matze@braunis.de>
2011-04-12 13:12:50 +0200

    testcase demonstrating wrong pure/const detection

---
+++ opt/fehler186.c
@@ -0,0 +1,18 @@
+/*$ -fno-opt-tail-rec $*/
+
+extern int printf(const char *str, ...);
+
+int __attribute__((noinline)) sum(int k)
+{
+	/* firm incorrectly marked sum as a pure function and then
+	 * if-converted this leading to an endless recursion */
+	if (k<=0)
+		return 0;
+	return k+sum(k-1);
+}
+
+int main(void)
+{
+	printf("%d %d\n", sum(-1), sum(10));
+	return 0;
+}
