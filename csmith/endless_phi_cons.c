7e4769f9962e576c47dbfad5b30435365f136bd5
Christoph Mallon <christoph.mallon@gmx.de>
2011-04-15 09:28:54 +0200

    Endless recursion in phi construction.

---
+++ csmith/endless_phi_cons.c
@@ -0,0 +1,14 @@
+typedef unsigned short int uint16_t;
+
+void func_51(void)
+{
+    return;
+    for (uint16_t l_248 = 0; (l_248 <= 45); ++l_248)
+    {
+    }
+}
+
+int main (int argc, char* argv[])
+{
+	return 0;
+}
