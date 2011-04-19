4943bcb4cdf9d2c9cd119431e92a31ea9584d7c8
Andreas Zwinkau <zwinkau@kit.edu>
2011-04-19 18:44:25 +0200

    Moved failing csmith test badcall into backend/

---
+++ backend/lower_call2.c
@@ -0,0 +1,15 @@
+struct S0 {
+   int f0;
+};
+
+struct S0 func_97(long long p_99)
+{
+    struct S0 l_101 = {-7L};
+    return l_101;
+}
+
+int main (int argc, char* argv[])
+{
+    func_97(0);
+    return 0;
+}
