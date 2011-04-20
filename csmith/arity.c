69ae1fa45b9c7e8338449a35644f9d5a2e563025
Andreas Zwinkau <zwinkau@kit.edu>
2011-04-20 11:32:53 +0200

    Simplifying csmith/arity.c even more

--- csmith/arity.c
+++ csmith/arity.c
@@ -1,49 +1,38 @@
-typedef int int32_t;
-typedef unsigned short int uint16_t;
-typedef unsigned int uint32_t;
-
 struct S0 {
-   uint32_t f0;
-   uint32_t f1;
-   uint16_t f4;
+	int f1;
+	int f4;
 };
 
-
-int32_t g_93 = 0L;
-struct S0 g_103 = {0U,0xA4B52006L,0x4C00L};
-
-void func_51(uint16_t p_52)
+void func_51(int p_52)
 {
-    for (int32_t g_61 = 0; g_61 < 8; g_61 += 7)
-    {
-        uint16_t l_248 = 0x9B5DL;
-        for (g_103.f0 = 0; (g_103.f0 >= 47); ++g_103.f0)
-        {
-            for (g_103.f4 = 3; g_103.f4 < 8; g_103.f4 += 8)
-            {
-                return;
-            }
-            if ((p_52 != 0) / l_248)
-            {
-                for (g_103.f4 = 0; g_103.f4 < 5; g_103.f4 += 4)
-                {
-                    for (g_103.f1 = 0; g_103.f1 < 2; g_103.f1 += 1)
-                    {
-                    }
-                }
-            }
-            else
-            {
-                for (l_248 = 0; (l_248 <= 45); ++l_248)
-                {
-                    g_93 = 0;
-                }
-            }
-        }
-    }
+	struct S0 g_103 = {0U,0x4C00L};
+	for (int g_61 = 0; g_61 < 8; g_61 += 7)
+	{
+		int l_248 = 0x9B5DL;
+		for (g_103.f1 = 0; (g_103.f1 >= 47); ++g_103.f1)
+		{
+			for (g_103.f4 = 3; g_103.f4 < 8; g_103.f4 += 8)
+			{
+				return;
+			}
+			if ((p_52 != 0) + l_248)
+			{
+				for (g_103.f4 = 0; g_103.f4 < 5; g_103.f4 += 4)
+				{
+					for (g_103.f1 = 0; g_103.f1 < 2; g_103.f1 += 1)
+					{
+					}
+				}
+			}
+			for (l_248 = 0; (l_248 <= 45); ++l_248)
+			{
+				g_103.f1 = 0;
+			}
+		}
+	}
 }
 
 int main (int argc, char* argv[])
 {
-    return 0;
+	return 0;
 }
