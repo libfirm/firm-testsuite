38dee936b541bdf10882b1d9688184dc3e2dacd3
Christoph Mallon <christoph.mallon@gmx.de>
2011-04-20 08:59:22 +0200

    Use the value from a variable instead of duplicating it.

--- external/firm-1.18.sh
+++ external/firm-1.18.sh
@@ -8,11 +8,11 @@ set -e
 FIRMVERSION="1.18.0"
 grab "http://switch.dl.sourceforge.net/sourceforge/libfirm/libfirm-$FIRMVERSION.tar.bz2"
 CPARSERVERSION="0.9.11"
-grab "http://switch.dl.sourceforge.net/sourceforge/cparser/cparser-0.9.11a.tar.bz2"
+grab "http://switch.dl.sourceforge.net/sourceforge/cparser/cparser-${CPARSERVERSION}a.tar.bz2"
 pushd $BUILDDIR > /dev/null
 echo "Extracting..."
 tar -xf "$PACKAGEDIR/libfirm-$FIRMVERSION.tar.bz2"
-tar -xf "$PACKAGEDIR/cparser-0.9.11a.tar.bz2"
+tar -xf "$PACKAGEDIR/cparser-${CPARSERVERSION}a.tar.bz2"
 cd "libfirm-$FIRMVERSION"
 CC=cparser ./configure --prefix=/tmp/firmtest
 make $MAKEFLAGS
