9dbe601a7ff32c07c7a4bc18b070ab673df740d2
Andreas Zwinkau <zwinkau@kit.edu>
2011-04-20 15:59:24 +0200

    Add failing opt/doms_segfault.c

---
+++ opt/doms_segfault.c
@@ -0,0 +1,28 @@
+int g_7 = (-1L);
+int g_96 = 0x91D145C1L;
+
+int func_25( int p_27, int p_28, int * p_29, int * p_30)
+{
+	for (p_27 = 0; (p_27 < (-2)); p_27 = 1)
+	{
+		continue;
+	}
+	for (g_96 = 9; (g_96 == 5); g_96 = (1))
+	{
+		for (g_96 = 9; (g_96 == 5); g_96 = (1))
+		{
+			for (g_7 = 0; g_7 < 5; g_7 += 3)
+			{
+				return 0;
+			}
+		}
+		return 0;
+	}
+	for (;;) ;
+	return 0;;
+}
+
+int main (int argc, char* argv[])
+{
+	return 0;
+}
