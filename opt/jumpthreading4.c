fbb48fe970892044c81394bd5d9327fae565b100
Andreas Zwinkau <zwinkau@kit.edu>
2011-04-21 14:45:46 +0200

    Add new jumpthreading test
    Already fixed as of libfirm aa4487c

---
+++ opt/jumpthreading4.c
@@ -0,0 +1,167 @@
+
+struct S0 {
+   int f0;
+   int f1;
+   int f2;
+   unsigned long long f3;
+  int f4;
+   int f5;
+   int f6;
+};
+
+
+int g_2 = (-1L);
+int * const  g_23 = 0;
+int g_43 = 0x5C2F4FE1L;
+int *  g_58 = &g_43;
+const int **g_60 = 0;
+int *g_64[7] = {&g_2, &g_2, &g_2, &g_2, &g_2, &g_2, &g_2};
+int **g_63 = &g_64[3];
+int g_84 = 0x332E7A53L;
+ struct S0 g_93 = {0L,0x53A1071FL,9U,18446744073709551608U,0x919EL,0x48480AD74ECB9A7BLL,-1L};
+ struct S0 *  g_94 = 0;
+struct S0 g_109 = {-7L,4294967295U,0x45424915L,0x248422EE889C39DBLL,1L,18446744073709551606U,0L};
+struct S0 *  g_110 = 0;
+struct S0 g_111 = {-1L,4294967295U,3U,0xA86934388704077ELL,0xF98DL,18446744073709551615U,0L};
+ struct S0 *  g_142 = 0;
+int g_152 = 0L;
+int *  g_151 = &g_152;
+ struct S0 g_157 = {};
+ struct S0 *  g_158 = &g_93;
+struct S0 *g_164 = 0;
+struct S0 **  g_163 = &g_164;
+struct S0 g_181 = {};
+ int g_274 = 0xBD085EF9L;
+ int *g_273 = &g_274;
+struct S0 g_275[10] = {{}};
+struct S0 g_277 = {};
+struct S0 g_325 = {};
+ struct S0 g_335 = {};
+int g_346 = 4294967295U;
+int *  g_379 = &g_2;
+const int *g_383 = &g_2;
+ struct S0 ***g_465 = 0;
+
+int * func_18(unsigned p_19, const int * const p_20, unsigned p_21)
+{
+    int l_366 = 1L;
+    int *l_439 = &g_84;
+    int *l_480[9][7][1];
+    int i, j, k;
+    for (i = 0; i < 9; i++)
+    {
+        for (j = 0; j < 7; j++)
+        {
+            for (k = 0; k < 1; k++)
+                l_480[i][j][k] = &l_366;
+        }
+    }
+    if ((*p_20))
+    {
+        int *l_24[2][8];
+        unsigned l_367 = 0xAB1B619053269671LL;
+        struct S0 **l_456 = 0;
+        int i, j;
+        {
+            for (j = 0; j < 8; j++)
+                l_24[i][j] = 0;
+        }
+        if (((((l_24[1][6] != l_24[1][6]) & (p_21 == p_21)), ((g_157.f3, 12)))))
+        {
+            unsigned l_369 = 0xB6D0;
+            unsigned l_403 = 8U;
+            if ((((g_346 && 1L), (((0U <= ((g_277.f2, 0U))), (((g_111.f0 < (((g_335.f4 || p_21)))), (((p_19, (((*p_20))))) < p_21))))))))
+            {
+                int **l_359 = &g_64[3];
+                int i, j;
+                if ((*p_20))
+                {
+                    int *l_358 = &g_84;
+                    int i;
+                    for (g_109.f3 = 4; g_109.f3 < 10; g_109.f3 += 3)
+                    {
+                        l_366 |= (!(((l_359) > 1) && 0x9AF5L));
+                    }
+                    if (l_367)
+                    {
+                        int **l_368 = 0;
+                        (*l_368) = (g_2);
+                    }
+                    {
+                        int ** const l_378 = &l_358;
+                        l_366 = 0;
+                        (*g_379) &= ((&l_24[0][0]) > 0);
+                    }
+                }
+                else
+                {
+						  return 0;
+                }
+            }
+            else
+            {
+                struct S0 **l_399 = &g_164;
+                if ((g_275[1].f3))
+                {
+                    for (g_181.f3 = (-25); (g_181.f3 >= 1); g_181.f3 = (g_181.f3, 3))
+                    {
+                        return 0;
+                    }
+                    if (5)
+                    {
+                    }
+                        (&g_142 != l_399);
+                        (*g_273) = (((( ((p_19 >= g_325.f0))) || (0x10L && ((l_403)))), g_277.f0));
+                        goto lbl_409;
+                }
+                else
+                {
+lbl_409:
+                    return 0;
+                }
+            }
+            if ((*g_383)) { }
+            {
+                for (g_109.f4 = (-7); (g_109.f4 >= 2); g_109.f4 = (g_109.f4, 1))
+                {
+                }
+                    int *l_445[6];
+                    int i;
+                    for (i = 0; i < 6; i++)
+                        l_445[i] = 0;
+                    for (g_181.f1 = (-8); (g_181.f1 != 55); g_181.f1 = (g_181.f1, 1))
+                    {
+                    }
+            }
+        }
+        else
+        {
+lbl_468:
+            l_456 = l_456;
+        }
+        {
+            int **l_469[3];
+            int i, j, k;
+            for (i = 0; i < 3; i++)
+				{
+					for (i = 0; i < 3; i++)
+					{
+						for (i = 0; i < 3; i++)
+							l_469[i] = 0;
+					}
+				}
+            (*l_439) = (g_465 != &l_456);
+        }
+            goto lbl_468;
+    }
+    else
+    {
+        for (;;) { }
+    }
+    return 0;
+}
+
+int main (int argc, char* argv[])
+{
+    return 0;
+}
