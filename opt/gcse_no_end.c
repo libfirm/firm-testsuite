64f3bae58cb20377c5fcc120a70aea5b5fa6223b
Andreas Zwinkau <zwinkau@kit.edu>
2011-05-05 10:16:58 +0200

    Add testcase
    Fixed as of libfirm 6bedfc5.
    End became unreachable, so an assert in gcse was triggered.
    However, the actual bug was in jump threading.

---
+++ opt/gcse_no_end.c
@@ -0,0 +1,17 @@
+/* originally from csmith 2.0.0 with seed 816295358 */
+
+int g_3[1] = {};
+
+int func_1(void)
+{
+    for (g_3[0] = 5; ; g_3[0] += 1)
+    {
+        g_3[0] = g_3[0] > 4;
+    }
+    return 0;
+}
+
+int main (int argc, char* argv[])
+{
+    return 0;
+}
