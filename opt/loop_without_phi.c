d47593948ba4679fa4a3aa979a6119eaaca08d45
Andreas Zwinkau <zwinkau@kit.edu>
2011-05-05 11:28:20 +0200

    Add opt/loop_without_phi
    Fixed as of libfirm bcaf3e7

---
+++ opt/loop_without_phi.c
@@ -0,0 +1,26 @@
+/* originally from csmith 2.0.0 with seed 3345708674 */
+
+int * func_72(int p_73)
+{
+	int l_84[9];
+	int i, j, k;
+	for (;;)
+	{
+		if (p_73)
+		{
+			int l_83 = 0;
+			if (p_73)
+				break;
+			for (l_83 = 0; l_83 < 9; l_83 += 1)
+			{
+				(p_73) = l_84[0];
+			}
+		}
+	}
+	return 0;
+}
+
+int main (int argc, char* argv[])
+{
+    return 0;
+}
