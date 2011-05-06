640829918575707e3eb3764dab9ae2e11fbcb4b9
Andreas Zwinkau <zwinkau@kit.edu>
2011-05-06 13:41:08 +0200

    Add opt/inline_infinite_loop
    Fixed as of libfirm 8817a5b

---
+++ opt/inline_infinite_loop.c
@@ -0,0 +1,23 @@
+/* originally from csmith 2.0.0 with seed 1479486932 */
+
+int func_55()
+{
+	for (;;) {}
+	return 0;
+}
+
+int func_25()
+{
+    return 0;
+}
+
+int func_41()
+{
+	func_25() && func_55();
+	return 0;
+}
+
+int main (int argc, char* argv[])
+{
+    return 0;
+}
