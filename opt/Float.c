c0a647fdc8f371f7714c41dd3a86f9f2ee300eeb
Christoph Mallon <christoph.mallon@gmx.de>
2011-05-10 09:53:21 +0200

    Change opt/Float.c so the result does not depend on the textual representantions of (DBL|FLT)_(MAX|MIN).

--- opt/Float.c
+++ opt/Float.c
@@ -20,9 +20,6 @@ static void compute (double d1) {
   printf(" expecting 123456789012345678.0: %19.1f\n", d3);
 }
 
-#define _p(x)	#x
-#define p(x)	_p(x)
-
 int main (int argc, char *argv[]) {
     float fminplus  = +FLT_MIN;
     float fminminus = -FLT_MIN;
@@ -34,16 +31,16 @@ int main (int argc, char *argv[]) {
     double dmaxminus = -DBL_MAX;
 
     printf("\nExtreme representable float values:\n");
-    printf(" +" p(FLT_MIN) " = %+1.8E\n", fminplus);
-    printf(" -" p(FLT_MIN) " = %+1.8E\n", fminminus);
-    printf(" +" p(FLT_MAX) " = %+1.8E\n", fmaxplus);
-    printf(" -" p(FLT_MAX) " = %+1.8E\n", fmaxminus);
+    printf(" +FLT_MIN = %+1.8E\n", fminplus);
+    printf(" -FLT_MIN = %+1.8E\n", fminminus);
+    printf(" +FLT_MAX = %+1.8E\n", fmaxplus);
+    printf(" -FLT_MAX = %+1.8E\n", fmaxminus);
 
     printf("\nExtreme representable double values:\n");
-    printf(" +" p(DBL_MIN) " = %+1.17E\n", dminplus);
-    printf(" -" p(DBL_MIN) " = %+1.17E\n", dminminus);
-    printf(" +" p(DBL_MAX) " = %+1.17E\n", dmaxplus);
-    printf(" -" p(DBL_MAX) " = %+1.17E\n", dmaxminus);
+    printf(" +DBL_MIN = %+1.17E\n", dminplus);
+    printf(" -DBL_MIN = %+1.17E\n", dminminus);
+    printf(" +DBL_MAX = %+1.17E\n", dmaxplus);
+    printf(" -DBL_MAX = %+1.17E\n", dmaxminus);
 
     compute(1234567890000000000.0);
 
