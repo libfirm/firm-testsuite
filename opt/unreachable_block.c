13de0936a1502f188c249a4524d61b1ff8278dda
Andreas Zwinkau <zwinkau@kit.edu>
2011-05-16 12:48:37 +0200

    Add opt/unreachable_block
    Fixed as of libfirm c64fe29

---
+++ opt/unreachable_block.c
@@ -0,0 +1,19 @@
+/** originally from csmith 2.0.0 with seed 3069999580 */
+
+int g_27 = 0;
+
+void func_23(int p_25)
+{
+    int l_26[1];
+    short l_31 = 0xE1B8;
+    if (((l_31 >= ((&g_27 == &l_26))) && (&g_27 != 0)) || (&l_26[(unsigned short)(p_25)]))
+    {
+        int *l_35 = &l_26[(unsigned short)(p_25)];
+        (*l_35) = 0;
+    }
+}
+
+int main (int argc, char* argv[])
+{
+    return 0;
+}
