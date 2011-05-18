9bd7b018676e2f7fc3e16779f5e32cb297137df8
Andreas Zwinkau <zwinkau@kit.edu>
2011-05-18 12:32:36 +0200

    Add opt/jumpthreading7

---
+++ opt/jumpthreading7.c
@@ -0,0 +1,22 @@
+/* originally from csmith 2.0.0 with seed 841114199 */
+
+static int
+(safe_add_func_uint32_t_u_u)(int ui1, int ui2 )
+{
+  return ui1 + ui2;
+}
+
+int func_83()
+{
+	short l_91 = 0;
+	for (l_91 = 0; l_91 != 5; l_91 = safe_add_func_uint32_t_u_u(l_91, 4))
+	{
+		l_91 = 4;
+	}
+	return 0;
+}
+
+int main (int argc, char* argv[])
+{
+    return 0;
+}
