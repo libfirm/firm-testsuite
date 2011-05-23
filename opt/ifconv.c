5f704093150f2c35ce818dc254f26aecff1a6a25
Andreas Zwinkau <zwinkau@kit.edu>
2011-05-23 12:54:35 +0200

    Add opt/ifconv.c
    ifconv's smart walk-and-change just does not work with crazy Keep edges.

---
+++ opt/ifconv.c
@@ -0,0 +1,24 @@
+/* originally csmith 2.0.0 with seed 4119682785 */
+
+int (safe_add_func_uint64_t_u_u)(int ui1, int ui2 )
+{
+  return ui1 + ui2;
+}
+
+int g_56 = 0xAE;
+
+void func_85()
+{
+    int *l_90 = 0;
+    {
+        for (g_56 = 0; (g_56 != (-23)); )
+        {
+            (*l_90) = (1 < (safe_add_func_uint64_t_u_u(0, 0))) != 0;
+        }
+    }
+}
+
+int main (int argc, char* argv[])
+{
+    return 0;
+}
