726202b167ee90866c311aefbc0cab1223f487a4
Andreas Zwinkau <zwinkau@kit.edu>
2011-05-23 14:51:40 +0200

    Add opt/localopts2

---
+++ opt/localopts2.c
@@ -0,0 +1,33 @@
+/* originally from csmith 2.0.0 with seed 1999897674 */
+
+unsigned char
+(safe_sub_func_uint8_t_u_u)(int ui1, int ui2 )
+{
+  return ui1 - ui2;
+}
+
+int g_32 = 0L;
+
+int * func_11(int p_13)
+{
+    int *l_16 = &g_32;
+    int **l_15 = &l_16;
+    return 0;
+}
+
+int func_44()
+{
+    if (1)
+    {
+        for (g_32 = 0; (g_32 != (-12)); g_32 = safe_sub_func_uint8_t_u_u(g_32, 9))
+        {
+            func_11(func_11(g_32));
+        }
+    }
+    return 0;
+}
+
+int main (int argc, char* argv[])
+{
+    return 0;
+}
