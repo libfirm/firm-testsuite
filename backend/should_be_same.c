462715d1f0d326848130d25e68bc13bb24af6f15
Christoph Mallon <christoph.mallon@gmx.de>
2011-05-24 18:41:27 +0200

    Simplify testcase.

--- backend/should_be_same.c
+++ backend/should_be_same.c
@@ -8,19 +8,18 @@ int (safe_mul_func_int16_t_s_s)(short si1, short si2)
 
 long long (safe_unary_minus_func_int64_t_s)(long long si)
 {
-	return (si==(-9223372036854775807LL -1)) ?  ((si)) : -si;
+	return si == (-9223372036854775807LL -1) ?  si : -si;
 }
 
-int g_3 = 0x70;
 int g_236[9] = {};
 
 long long func_63(int p_64)
 {
 	int *l_388 = 0;
-	int l_397[5] = {};
-	for (g_3 = 0; g_3 < 9; g_3 += 1)
+	int l_397[5] = { 0 };
+	for (int g_3 = 0; g_3 < 9; g_3 += 1)
 	{
-		((((((safe_mul_func_int16_t_s_s(p_64, g_3)) <= (safe_unary_minus_func_int64_t_s(g_236[(g_3) % 9]))))) ^ (*l_388)) && l_397[3]);
+		((safe_mul_func_int16_t_s_s(p_64, g_3) <= safe_unary_minus_func_int64_t_s(g_236[g_3])) ^ *l_388) && l_397[3];
 	}
 	return 0;
 }
