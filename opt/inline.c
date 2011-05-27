7a6b2a7e214372cce96fddc124a6e041b3c03c4e
Andreas Zwinkau <zwinkau@kit.edu>
2011-05-27 19:30:41 +0200

    Add opt/inline

---
+++ opt/inline.c
@@ -0,0 +1,32 @@
+/* originally from csmith 2.0.0 with seed 3733772947 */
+
+struct S0 { };
+
+int func_4(struct S0 p_6)
+{
+	return 0;
+}
+
+int func_60(int p_61)
+{
+	int l_65[1] = {0xB5L};
+	return 0;
+}
+
+struct S0 g_216 = {};
+
+int func_49(int p_50)
+{
+	func_60(func_4(g_216));
+	return 0;
+}
+
+void func_45()
+{
+	func_49(0);
+}
+
+int main (int argc, char* argv[])
+{
+	return 0;
+}
