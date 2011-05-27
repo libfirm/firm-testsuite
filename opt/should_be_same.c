5339d6347589be2351db397221bfb504244bf952
Andreas Zwinkau <zwinkau@kit.edu>
2011-05-27 19:00:38 +0200

    Add opt/should_be_same
    csmith rediscovered the should_be_same bug

---
+++ opt/should_be_same.c
@@ -0,0 +1,17 @@
+/* originally from csmith 2.0.0 with seed 3973658307 */
+/*$ cflags -fno-ifconversion $*/
+
+long long (safe_sub_func_uint64_t_u_u)(long long ui1, int ui2 )
+{
+	return ui1 - ui2;
+}
+
+void func_9(int p_10)
+{
+	(((p_10 || (safe_sub_func_uint64_t_u_u((1 == p_10), 1)))));
+}
+
+int main (int argc, char* argv[])
+{
+    return 0;
+}
