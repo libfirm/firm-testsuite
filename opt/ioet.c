88310e75ab7d8383b9cb13828ccb65d09c2ba0c7
Andreas Zwinkau <zwinkau@kit.edu>
2011-05-31 12:57:43 +0200

    Add opt/ioet
    fixed as of libFirm c9665fa

---
+++ opt/ioet.c
@@ -0,0 +1,13 @@
+/* from et be_aas_route.c */
+
+void AAS_RecordTeamDeathArea( float srcpos[3], int srcarea, int team, int teamCount, int travelflags) {
+	int numreach = 10;
+
+	return;
+
+	for (int i = 0; i < numreach; i++);
+}
+
+int main(void) {
+	return 0;
+}
