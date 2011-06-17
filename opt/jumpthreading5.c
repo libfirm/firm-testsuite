d7879485246b3d8a409be51bec2c28be96d7d9bf
Christoph Mallon <christoph.mallon@gmx.de>
2011-06-17 11:24:28 +0200

    Merge jumpthreading5.c and jumpthreading5b.c (one infinite loop is enough) and update fail_expectations.

--- opt/jumpthreading5.c
+++ opt/jumpthreading5.c
@@ -23,7 +23,22 @@ lbl_159:
 	return 0;
 }
 
+void f(int x)
+{
+	if (!x) {
+		x = 8;
+	}
+	while (x < 10)
+	{
+		while (x > 5)
+		{
+		}
+	}
+	return;
+}
+
 int main (int argc, char* argv[])
 {
+	f(12);
 	return 0;
 }
