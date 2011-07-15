142c4def73eccf09a13622e6edd6449cb7a351c6
Sebastian Buchwald <Sebastian.Buchwald@kit.edu>
2011-07-15 13:52:30 +0200

    Extended testcase.

--- opt/fehler191.c
+++ opt/fehler191.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 
 unsigned x  = 0x0FFFFFFF;
+unsigned y  = 0xFFFFFFFF;
 
 void f(unsigned x)
 {
@@ -11,9 +12,19 @@ void f(unsigned x)
 	printf("(0x%x >> %u) | 0x%x = 0x%x\n", x, c1, c2, res);
 }
 
+void g(unsigned x)
+{
+	unsigned c1 = 1;
+	unsigned c2 = 0x80000000;
+	unsigned res = (x >> c1) ^ c2;
+
+	printf("(0x%x >> %u) ^ 0x%x = 0x%x\n", x, c1, c2, res);
+}
+
 int main(void)
 {
 	f(x);
+	g(y);
 
 	return 0;
 }
