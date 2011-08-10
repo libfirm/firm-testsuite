3a580c5afcc375d712bad037df1e54894a3d9faa
Andreas Zwinkau <zwinkau@kit.edu>
2011-08-10 11:23:46 +0200

    csmith cought a regression in ast2firm

---
+++ C/folderror.c
@@ -0,0 +1,12 @@
+/* originally from csmith 2.0.0 with seed 1081529185
+ * and options: --max-funcs 2 --max-struct-fields 2 --max-block-size 2↵
+ */
+unsigned func_1(void)
+{
+	return 4294967292U > 0x7FA8428C9742AFDELL;
+}
+
+int main (int argc, char* argv[])
+{
+    return 0;
+}
