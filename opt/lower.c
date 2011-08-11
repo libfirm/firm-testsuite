0eade369b4f75d9bcb2da814ab7fb28c4c4144da
Andreas Zwinkau <zwinkau@kit.edu>
2011-08-11 16:13:26 +0200

    yet another csmith program
    bug already fixed as of libFirm a63fa62

---
+++ opt/lower.c
@@ -0,0 +1,24 @@
+/* originally from csmith 2.0.0 with seed 566742155
+ * Options:   --max-funcs 2 --max-struct-fields 2 --max-block-size 2
+ */
+
+int x;
+
+void func_16(int p_20, int p_21)
+{
+	for (p_21 = 0; p_21; )
+	{
+		x = *&p_20;
+	}
+}
+
+long long func_1(void)
+{
+	func_16(0, 0);
+	return 0;
+}
+
+int main (int argc, char* argv[])
+{
+    return 0;
+}
