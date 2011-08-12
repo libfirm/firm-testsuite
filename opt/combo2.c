911b67e89346c46889a52433b99fee802aebbe38
Andreas Zwinkau <zwinkau@kit.edu>
2011-08-12 13:45:38 +0200

    Another csmith test: combo bug

---
+++ opt/combo2.c
@@ -0,0 +1,24 @@
+/* originally from csmith 2.0.0 with seed 3723471297
+ * Options:   --max-funcs 2 --max-struct-fields 2 --max-block-size 2
+ */
+
+int g_26[1][1] = {{0}};
+
+static short (safe_sub_func_int16_t_s_s)(short si1, short si2 )
+{
+  return (si1 - si2);
+}
+
+
+void func_1(void)
+{
+	short l_61 = 0;
+	for (; (l_61 >= -4); l_61 = safe_sub_func_int16_t_s_s(l_61, 5))
+		for (l_61 = 0; l_61 < 1; l_61 += 1)
+			g_26[0][0];
+}
+
+int main (int argc, char* argv[])
+{
+    return 0;
+}
