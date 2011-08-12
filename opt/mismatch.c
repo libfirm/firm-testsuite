5c5cfe1d3e49b1a1dd77b434778a86b3ef32f8e9
Andreas Zwinkau <zwinkau@kit.edu>
2011-08-12 16:03:35 +0200

    an output mismatch bug from csmith

---
+++ opt/mismatch.c
@@ -0,0 +1,20 @@
+/*
+ * Generator: csmith 2.0.0
+ * Options:   --max-funcs 1 --max-struct-fields 2 --max-block-size 1 --bitfields --max-array-len-per-dim 3 --paranoid
+ * Seed:      3089896088
+ */
+
+#include <stdio.h>
+
+short g_8 = 1L;
+
+unsigned short inv(unsigned short x) {
+	return -x;
+}
+
+int main (int argc, char* argv[])
+{
+	int x = (inv(0x9) >= g_8);
+	printf("%d\n", x);
+	return 0;
+}
