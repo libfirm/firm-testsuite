1fdea447acb0b9cf9f85ea0311a574e0f356aeac
Christoph Mallon <christoph.mallon@gmx.de>
2011-08-15 16:18:00 +0200

    Correct spurious warning. The expected warning is not produced for this testcase.

--- C/should_warn/paramscope.c
+++ C/should_warn/paramscope.c
@@ -1,6 +1,6 @@
 /* should warn about "struct x" being only defined/known inside the parameter list */
-static int test(struct x *p) {
-	return sizeof(p);
+static void test(struct x *p) {
+	(void)p;
 }
 
 int main(void) {
