a3e75dccba0fd951913e8776b1f24ed00d0ae66f
Andreas Zwinkau <zwinkau@kit.edu>
2011-08-15 14:19:54 +0200

    Add C/bitfield4 fail
    Semantics mismatch between front- and middleend.
    Frontend uses Sel-Load-Shl-Shrs for bitfield accesses.
    Load-Store-Opt sees Sel-Load and replaces it with field value.

---
+++ C/bitfield4.c
@@ -0,0 +1,20 @@
+/*
+ * Original Generator: csmith 2.0.0
+ * svn version: exportiert
+ * Options:   --max-funcs 1 --max-struct-fields 2 --max-block-size 1 --bitfields --max-array-len-per-dim 3 --paranoid -o found/test000042.c
+ * Seed:      1382860393
+ */
+
+struct S0 {
+	int f0:5;
+	int f1:5;
+};
+
+const struct S0 g_5 = {1,2};
+
+int main (void)
+{
+	printf("%d\n", g_5.f0);
+	printf("%d\n", g_5.f1);
+	return 0;
+}
