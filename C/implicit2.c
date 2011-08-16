14c4205ddcdc5c6f18ac47c36316ba1c56b0bc0b
Andreas Zwinkau <zwinkau@kit.edu>
2011-08-16 16:46:20 +0200

    Another frontend bug found by csmith

---
+++ C/implicit2.c
@@ -0,0 +1,14 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.0.0
+ * svn version: exportiert
+ * Options:   --max-funcs 1 --max-struct-fields 2 --max-block-size 1 --bitfields --max-array-len-per-dim 3 --paranoid -o found/test001442.c
+ * Seed:      1601771491
+ */
+
+int main (int argc, char* argv[])
+{
+	printf("%d\n", (int)(~((unsigned short)3)));
+	return 0;
+}
