ec6c90a00a79573fbf33ab1bbc65168fc8b80b37
Andreas Zwinkau <zwinkau@kit.edu>
2011-08-17 14:10:04 +0200

    Another output mismatch by csmith

---
+++ C/init8.c
@@ -0,0 +1,16 @@
+/*
+ * Generator: csmith 2.0.0
+ * svn version: exportiert
+ * Options:   --max-funcs 1 --max-struct-fields 2 --max-block-size 1 --bitfields --max-array-len-per-dim 3 --paranoid -o found/test003206.c
+ * Seed:      995762732
+ */
+
+#include <stdio.h>
+
+char g_2[3] = {0U, 0x38L, 0U};
+
+int main (void)
+{
+	printf("%d\n", g_2[0]);
+	return 0;
+}
