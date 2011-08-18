e7412170e5f7f97235c84c253afeaf95af277d49
Sebastian Buchwald <Sebastian.Buchwald@kit.edu>
2011-08-18 13:31:21 +0200

    Added another output mismatch test found by csmith.

---
+++ opt/fehler196.c
@@ -0,0 +1,20 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.0.0
+ * svn version: exportiert
+ * Options:   --max-funcs 1 --max-struct-fields 2 --max-block-size 2 --bitfields --max-array-len-per-dim 2 --paranoid -o found/test009899.c
+ * Seed:      732329381
+ */
+#include <stdio.h>
+
+int g_6 = 3;
+
+int main (int argc, char* argv[])
+{
+    const int l_7 = 4;
+    int g_14 = (-1L != (g_6 | l_7) % l_7) | (0L == (g_6 || (g_6 >> g_6))) != (l_7 < g_6);
+
+    printf("%d\n", g_14);
+    return 0;
+}
