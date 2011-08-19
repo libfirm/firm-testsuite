efddf80bad0a56dd946306466124f0506f4267f5
Sebastian Buchwald <Sebastian.Buchwald@kit.edu>
2011-08-19 14:18:04 +0200

    Test for bug fixed in e6a5a50a41ad84093d66a81994a8b0d276baa113.

---
+++ opt/fehler198.c
@@ -0,0 +1,30 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.0.0
+ * svn version: exportiert
+ * Options:   --max-funcs 1 --max-struct-fields 2 --max-block-size 2 --bitfields --max-array-len-per-dim 2 --paranoid -o found/test031573.c
+ * Seed:      1937985357
+ */
+#include<stdio.h>
+
+/* --- GLOBAL VARIABLES --- */
+unsigned int g_4 = 1U;/* VOLATILE GLOBAL g_4 */
+unsigned int g_7 = 0x2L;
+int g_13 = 0x53589FE5L;
+int g_32 = 0L;
+
+static short safe_mod_func_int16_t_s_s(short si1, short si2 )
+{
+  return ((si2 == 0) || (si2 == (-1))) ? ((si1)) : (si1 % si2);
+}
+
+/* ---------------------------------------- */
+int main(void)
+{
+	g_32 = safe_mod_func_int16_t_s_s(-1 , ((1 <= g_7) && (g_13 >> 14)));
+
+	printf("%d\n", g_32);
+
+	return 0;
+}
