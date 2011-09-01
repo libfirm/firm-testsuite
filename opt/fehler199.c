a135803c313ff55540702b60f3851ef833ad8271
Sebastian Buchwald <Sebastian.Buchwald@kit.edu>
2011-09-01 15:21:34 +0200

    We generate Firm graphs with wrong semantic.
    CopyB lowering generates wrong modes for Load and Stores (in this case Iu instead of Hu) for a copy of a struct.
    Then the (strict) alias analysis parallelize memory access, because the store
    mode is Iu and the load mode is Hu (-> different -> do not alias).

---
+++ opt/fehler199.c
@@ -0,0 +1,32 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.0.0
+ * svn version: exportiert
+ * Options:   --max-funcs 1 --max-struct-fields 2 --max-block-size 2 --bitfields --max-array-len-per-dim 2 -o found/test010393.c
+ * Seed:      889793406
+ */
+#include <stdio.h>
+
+static unsigned int crc32_tab[2] = {0, 0};
+
+static unsigned int crc32_context = 0xFFFFFFFFUL;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned short  f0;
+   unsigned f1 : 17;
+};
+
+/* --- GLOBAL VARIABLES --- */
+struct S0 g_4 = {0x1L,304};
+struct S0 *g_3 = &g_4;
+
+/* ---------------------------------------- */
+int main (int argc, char* argv[])
+{
+	int print_hash_value = 0;
+	struct S0 l_2 = {0U,318};
+	(*g_3) = l_2;
+	return g_4.f0;
+}
