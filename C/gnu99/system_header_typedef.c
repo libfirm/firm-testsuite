bf17aa4ee01d0df1045628ad6e8efc5adb1ba0de
Matthias Braun <matze@braunis.de>
2011-09-05 18:30:33 +0200

    gcc has exceptions for system headers

---
+++ C/gnu99/system_header_typedef.c
@@ -0,0 +1,10 @@
+/* gcc produces no error if a typedef is repeated in a system header...
+ * (and the glibc/gcc headers appear to be broken enough to redfine size_t on
+ *  my system here) */
+typedef unsigned long BLABLA;
+# 1 "XXX" 1 3 4
+typedef unsigned long BLABLA;
+
+int main(void)
+{
+}
