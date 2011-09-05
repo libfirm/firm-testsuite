b2d2c11d17a62397fd05700670384acfeb02fcad
Matthias Braun <matze@braunis.de>
2011-09-05 19:43:09 +0200

    redundant decl warning tests

---
+++ C/nowarn/redundant_decl.c
@@ -0,0 +1,9 @@
+void exit(int status);
+# 1 "XXX" 3
+/* suppress redundant decl warnings in system headers */
+void exit(int status);
+
+int main(void)
+{
+	exit(0);
+}
---
+++ C/should_warn/redundant_decl.c
@@ -0,0 +1,7 @@
+void exit(int status);
+void exit(int status);
+
+int main(void)
+{
+	exit(0);
+}
