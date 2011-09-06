72fea03209c41170eed685fb6c98ba1e012dd63f
Matthias Braun <matze@braunis.de>
2011-09-06 13:03:18 +0200

    add another makereport mode for our leon sparc variant

--- makereport.py
+++ makereport.py
@@ -48,6 +48,15 @@ def setup_sparc(option, opt_str, value, parser):
 	config.runexe += "qemu-sparc32plus "
 	config.expect_file = "fail_expectations_sparc"
 
+def setup_leon(option, opt_str, value, parser):
+	global _ARCH_DIRS
+	_ARCH_DIRS = [ "sparccode" ]
+	config = parser.values
+	config.cflags += " -mtarget=sparc-leon-linux-gnu -msoft-float"
+	config.ldflags += " -static -msoft-float"
+	config.runexe += "qemu-sparc "
+	config.expect_file = "fail_expectations_sparc_leon"
+
 def setup_arm(option, opt_str, value, parser):
 	global _ARCH_DIRS
 	_ARCH_DIRS = [ "armcode" ]
@@ -97,6 +106,7 @@ _OPTS.add_option("", "--x10cflags", dest="x10cflags",
 _OPTS.add_option("", "--show-disappeared", dest="show_disappeared",
                  action="store_true", help="show disappeared tests")
 _OPTS.add_option("", "--sparc", action="callback", callback=setup_sparc)
+_OPTS.add_option("", "--leon", action="callback", callback=setup_leon)
 _OPTS.add_option("", "--arm", action="callback", callback=setup_arm)
 
 def ensure_dir(name):
