067c9640622bc9dc493bdbe8aba358b342e52096
Sebastian Buchwald <Sebastian.Buchwald@kit.edu>
2011-09-15 11:17:20 +0200

    Better test for the same bug.

--- opt/fehler199.c
+++ opt/fehler199.c
@@ -1,32 +1,18 @@
-/*
- * This is a RANDOMLY GENERATED PROGRAM.
- *
- * Generator: csmith 2.0.0
- * svn version: exportiert
- * Options:   --max-funcs 1 --max-struct-fields 2 --max-block-size 2 --bitfields --max-array-len-per-dim 2 -o found/test010393.c
- * Seed:      889793406
- */
-#include <stdio.h>
-
-static unsigned int crc32_tab[2] = {0, 0};
-
-static unsigned int crc32_context = 0xFFFFFFFFUL;
-
-/* --- Struct/Union Declarations --- */
 struct S0 {
-   unsigned short  f0;
-   unsigned f1 : 17;
+   short f0;
+   int   f1;
 };
 
-/* --- GLOBAL VARIABLES --- */
-struct S0 g_4 = {0x1L,304};
-struct S0 *g_3 = &g_4;
+static struct S0  global     = {1, 0x42};
+static struct S0 *global_ptr = &global;
 
-/* ---------------------------------------- */
-int main (int argc, char* argv[])
+int main(void)
 {
-	int print_hash_value = 0;
-	struct S0 l_2 = {0U,318};
-	(*g_3) = l_2;
-	return g_4.f0;
+    short     *global_f0_ptr = &global.f0;
+    struct S0  local         = {0, 0x42};
+
+    *global_f0_ptr = 42;
+    *global_ptr    = local;
+
+    return global.f0;
 }
