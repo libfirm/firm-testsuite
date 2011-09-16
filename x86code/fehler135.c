d2d51d0847e3b47d462908b91b462909972df000
Andreas Zwinkau <zwinkau@kit.edu>
2011-09-16 10:34:34 +0200

    Add x86code/fehler135.c

---
+++ x86code/fehler135.c
@@ -0,0 +1,16 @@
+/*
+ * This was a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.0.0
+ * Options:   --max-funcs 1 --max-struct-fields 3 --bitfields --max-block-size 1 --max-array-len-per-dim 1 -o found/test000105.c
+ * Seed:      3742332409
+ */
+
+unsigned long long g_6 = 1;
+int x = 1, y = 1;
+
+int main (int argc, char* argv[])
+{
+	x = (g_6 <= (y || x));
+	return 0;
+}
