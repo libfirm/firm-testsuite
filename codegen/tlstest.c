93cffc590ecab94f70027182d3d22de17f794b71
Matthias Braun <matze@braunis.de>
2011-09-21 16:44:08 +0200

    test thread local storage

---
+++ codegen/tlstest.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <pthread.h>
+
+__thread int tls_var;
+
+void *func1(void *ptr)
+{
+	(void)ptr;
+	tls_var = 42;
+	printf("1: TLS-Var before sleep: %d\n", tls_var);
+	sleep(1);
+	printf("1: TLS-Var after sleep: %d\n", tls_var);
+	sleep(2);
+}
+
+void *func2(void *ptr)
+{
+	(void)ptr;
+	tls_var = 13;
+	printf("2: TLS-Var before sleep: %d\n", tls_var);
+	sleep(2);
+	printf("2: TLS-Var after sleep: %d\n", tls_var);
+	sleep(1);
+}
+
+int main(void)
+{
+	pthread_t thread1;
+	pthread_t thread2;
+	printf("main: creating\n");
+	pthread_create(&thread1, NULL, func1, NULL);
+	pthread_create(&thread2, NULL, func2, NULL);
+	printf("main: joining...\n");
+
+	pthread_join(thread1, NULL);
+	pthread_join(thread2, NULL);
+	printf("main: all joined\n");
+	return 0;
+}
