069c91433ed5b5592dc1ec31f01ff1dcd9837284
Sebastian Buchwald <Sebastian.Buchwald@kit.edu>
2011-10-15 14:09:10 +0200

    We do not rematerialize flags.

---
+++ x86code/flags.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+static int global1 = 1L;
+static int global2 = 0L;
+static int global3 = 0L;
+
+int main(void)
+{
+	int result = 0;
+
+	if (global1 |= &global1 == &global2)
+	{
+		for (global3 = 0; global3 < 1; global3++)
+		{
+		}
+	}
+
+	printf("%d\n", global3);
+
+	return 0;
+}
