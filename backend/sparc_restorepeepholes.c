023e01b7e7b7ff517cc047c75bcff1334e04e8b6
Matthias Braun <matze@braunis.de>
2011-10-17 16:12:18 +0200

    test some fold-into-restore peephole opts on sparc

---
+++ backend/sparc_restorepeepholes.c
@@ -0,0 +1,45 @@
+/* a "restore" operations gives us a free "add" instruction, so the compiler
+ * should fold some instructions into it. */
+
+int g;
+
+extern int rand(); /* libc rand, just here so the compiler has a call it can't optimize away */
+
+unsigned __attribute__((noinline)) fold_const(void)
+{
+	g = 1;  /* so the call delay slots gets filled */
+	rand(); /* force save/restore usage */
+	return 42;
+}
+
+unsigned __attribute__((noinline)) fold_add(unsigned x, unsigned y)
+{
+	g = 1;
+	rand();
+	return x+y;
+}
+
+unsigned __attribute__((noinline)) fold_add_imm(unsigned x)
+{
+	g = 1;
+	rand();
+	return x + 42;
+}
+
+unsigned __attribute__((noinline)) fold_sub_imm(unsigned x)
+{
+	g = 1;
+	rand();
+	return x - 42;
+}
+
+extern int printf(const char *str, ...);
+
+int main(void)
+{
+	printf("1: %u\n", fold_const());
+	printf("2: %u\n", fold_add(13, 30));
+	printf("3: %u\n", fold_add_imm(2));
+	printf("4: %u\n", fold_sub_imm(87));
+	return 0;
+}
