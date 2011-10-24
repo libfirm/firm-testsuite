d4e5a7e9b87ff402e1fce06e30e2cdfccabda4c0
Andreas Zwinkau <zwinkau@kit.edu>
2011-10-24 16:27:28 +0200

    Add test for missing switch case in ia32 backend

---
+++ x86code/relation_false.c
@@ -0,0 +1,20 @@
+/*
+ * This was originally a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.0.0
+ * svn version: exportiert
+ * Options:   --max-funcs 2 --max-struct-fields 3 --max-block-size 3 --bitfields --no-packed-struct --max-array-len-per-dim 2 -o found/test000013.c
+ * Seed:      77566349
+ */
+
+long long f() {
+	return 0;
+}
+
+int main (void)
+{
+	int g_28 = 11;
+	int l_77[1] = {0};
+	g_28 = ((&g_28) && (l_77[0])) > f();
+	return 0;
+}
