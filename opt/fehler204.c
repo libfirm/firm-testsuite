b9270b9d603036ddffc02efe47a8601b4fc5311e
Andreas Zwinkau <zwinkau@kit.edu>
2011-10-26 10:53:50 +0200

    New csmith test case
    Fixed as of libfirm 73218d0

---
+++ opt/fehler204.c
@@ -0,0 +1,23 @@
+/*
+ * This was originally a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.0.0
+ * svn version: exportiert
+ * Options:   --max-funcs 3 --max-struct-fields 3 --max-block-size 3 --bitfields --no-packed-struct --max-array-len-per-dim 2 -o found/test000006.c
+ * Seed:      601116377
+ */
+
+int g_20;
+
+int main (int argc, char* argv[])
+{
+	for (g_20 = (-26); (g_20 >= (-2)); g_20++)
+	{
+		if (g_20)
+			return 0;
+		if (g_20)
+			break;
+		return 0;
+	}
+	return 0;
+}
