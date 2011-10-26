dca0a3b26756fa146fe77f08804ae906eeea836f
Andreas Zwinkau <zwinkau@kit.edu>
2011-10-26 16:57:23 +0200

    Add x86code/fehler136

---
+++ x86code/fehler136.c
@@ -0,0 +1,26 @@
+/*
+ * This was a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.1.0
+ * Git version: e65d3c7
+ * Options:   --max-funcs 2 --max-struct-fields 3 --max-block-size 3 --bitfields --no-packed-struct --max-array-len-per-dim 2 -o found/test000038.c
+ * Seed:      377202319
+ */
+
+struct S0 { };
+
+long long g_4 = 1UL;
+
+struct S0  func_2(int  p_3)
+{
+	const struct S0 l_11 = {};
+	return l_11;
+}
+
+int main (void)
+{
+	struct S0 x = func_2(g_4);
+	if (~(g_4 = (1 > g_4))) { }
+	else func_2(0);
+	return 0;
+}
