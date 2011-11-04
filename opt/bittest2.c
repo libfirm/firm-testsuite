18db11a5ce569387afad22bfb6b39557ba15abaa
Matthias Braun <matze@braunis.de>
2011-11-04 15:01:29 +0100

    another test for bitcmp stuff

---
+++ opt/bittest2.c
@@ -0,0 +1,27 @@
+extern int puts(const char *str);
+extern int printf(const char *str, ...);
+
+void __attribute__((noinline)) bittest(unsigned x)
+{
+	printf("Testing: %x\n", ~x);
+	/* the following shouldn't change the tested bits... and
+	 * could be left out by the compiler */
+	x = ~x;
+	x ^= 0x10;
+	x ^= 0x08;
+	x = ~x;
+
+	if (x & 8)
+		puts("0x08 is set");
+	if (x & 0x10)
+		puts("0x10 is set");
+}
+
+int main(void)
+{
+	bittest(~ 0);
+	bittest(~ 0xffffffffu);
+	bittest(~ 0x8);
+	bittest(~ 0x10);
+	bittest(~ 0x12345678);
+}
