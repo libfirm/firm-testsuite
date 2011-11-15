ba6beceded4f741e45d8173fab262216c3b1f014
Manuel Mohr <manuel.mohr@kit.edu>
2011-11-15 18:07:10 +0100

    Added test case for multiple consecutive allocas.

---
+++ backend/alloca.c
@@ -0,0 +1,13 @@
+#include <alloca.h>
+#include <assert.h>
+
+char *p1;
+char *p2;
+
+int main()
+{
+	/* SPARC backend managed to mess up multiple allocas. */
+	p1 = alloca(42);
+	p2 = alloca(42);
+	return p1 == p2;
+}
