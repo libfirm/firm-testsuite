85e5686712384dd9aaf571da60afd3021c0adf40
Sebastian Buchwald <Sebastian.Buchwald@kit.edu>
2011-12-07 14:45:04 +0100

    Adapted to latest version.

--- external/apache.sh
+++ external/apache.sh
@@ -3,7 +3,7 @@ set -e
 
 . "$(dirname "$0")/utils.sh"
 
-APACHEVERSION="2.2.17"
+APACHEVERSION="2.2.21"
 grab "http://ftp-stud.hs-esslingen.de/pub/Mirrors/ftp.apache.org/dist/httpd/httpd-$APACHEVERSION.tar.bz2"
 cd "$BUILDDIR"
 echo "Extracting..."
