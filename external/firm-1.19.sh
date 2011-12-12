b804a5934865af8dd57f8dc6489ec0f5af9beabf
Matthias Braun <matze@braunis.de>
2011-12-12 14:37:34 +0100

    update firm-1.19.1 script

--- external/firm-1.19.sh
+++ external/firm-1.19.sh
@@ -5,7 +5,7 @@ set -e
 
 . "$(dirname "$0")/utils.sh"
 
-FIRMVERSION="1.19.0"
+FIRMVERSION="1.19.1"
 grab "http://switch.dl.sourceforge.net/sourceforge/libfirm/libfirm-$FIRMVERSION.tar.bz2"
 CPARSERVERSION="0.9.12"
 grab "http://switch.dl.sourceforge.net/sourceforge/cparser/cparser-$CPARSERVERSION.tar.bz2"
@@ -14,6 +14,8 @@ echo "Extracting..."
 tar -xf "$PACKAGEDIR/libfirm-$FIRMVERSION.tar.bz2"
 tar -xf "$PACKAGEDIR/cparser-$CPARSERVERSION.tar.bz2"
 cd "libfirm-$FIRMVERSION"
+patch -p0 < "$TOPDIR/libfirm-1.19.1.patch"
+autoreconf
 CC=cparser ./configure --prefix=/tmp/firmtest
 make $MAKEFLAGS
 make install
