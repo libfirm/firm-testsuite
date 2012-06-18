e05748556c6e0e4eb88a6189999921145a32edce
Matthias Braun <matze@braunis.de>
2012-06-19 01:12:14 +0200

    some simple tests for the line directives

---
+++ preproctest/line_directive.c
@@ -0,0 +1,11 @@
+# 12 "foobar.blup"
+Im at line 12
+and 13
+# 65535 "another non existant file^^"
+Blup
+#line 65536 "another non existant file^^"
+bla
+# 13 "/usr/include/stdio.h" 3
+in the system
+# 17 "/usr/include/stdio.h"
+out of the system
