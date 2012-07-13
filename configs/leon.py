9ecd1f25d2f0d95f4c4358322a972ca7a4970189
Matthias Braun <matthias.braun@kit.edu>
2012-07-13 12:01:48 +0200

    testcase for digraphs

--- configs/leon.py
+++ configs/leon.py
@@ -4,7 +4,7 @@ def setup_leon(option, opt_str, value, parser):
 	config = parser.values
 	config.cflags += " -mtarget=sparc-leon-linux-gnu -msoft-float"
 	config.ldflags += " -static -msoft-float"
-	config.runexe = "qemu-sparc "
+	config.runexe = "qemu-sparc -r 2.6.40 "
 	config.expect_url = "fail_expectations_sparc_leon"
 
 configurations['leon'] = setup_leon
