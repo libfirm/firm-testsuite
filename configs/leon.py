0bcb04f56fea8d82b0cca7291d0609382b8256a5
Manuel Mohr <manuel.mohr@kit.edu>
2012-07-24 16:33:57 +0200

    Removed fail_expectations files, download them automatically.
    Having fail_expectations files in the testsuite repository is not a good
    idea because they are often outdated.  This commit makes makereport
    download the fail_expectations files as produced by the FIRM buildbot.
    The files are cached, so if no network is available, the last downloaded
    version is used.

--- configs/leon.py
+++ configs/leon.py
@@ -5,6 +5,6 @@ def setup_leon(option, opt_str, value, parser):
 	config.cflags += " -mtarget=sparc-leon-linux-gnu -msoft-float"
 	config.ldflags += " -static -msoft-float"
 	config.runexe = "qemu-sparc -r 2.6.40 "
-	config.expect_url = "fail_expectations_sparc_leon"
+	config.expect_url = "http://pp.info.uni-karlsruhe.de/cgi-bin/cgit.cgi/firm-testresults/plain/fail_expectations-sparc-leon-linux-gnu"
 
 configurations['leon'] = setup_leon
