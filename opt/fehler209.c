37c1fc5fad69281744e7dfe6fa45c1f73b5fe12d
Christoph Mallon <christoph.mallon@gmx.de>
2012-10-09 08:56:52 +0200

    Assertion in spiller found by csmith.

---
+++ opt/fehler209.c
@@ -0,0 +1,31 @@
+/*$ -O2 $*/
+typedef long long int int64_t;
+typedef unsigned int uint32_t;
+
+
+static void func_7(int64_t p_10, uint32_t p_11)
+{
+	if (0xA8L <= p_10) {
+		for (uint32_t g_426 = 0; g_426 <= 2; g_426 = g_426 + 8) {
+			uint32_t l_680[6];
+			for (int i = 0; i < 6; i++) {
+				l_680[i] = -10L;
+			}
+			for (int64_t g_472 = 3; g_472 >= 0; g_472 -= 1) {
+				for (int64_t g_546 = 0; g_546 <= 2; g_546 += 1) {
+					&p_11 == 0;
+				}
+				l_680[4] ^= 1;
+			}
+		}
+		for (int64_t g_437 = 0; g_437 >= -7; g_437 = 2) {}
+	}
+	for (int64_t l_644 = 0; l_644 <= 0; l_644++) {}
+}
+
+
+int main (int argc, char* argv[])
+{
+	func_7(0xCE6C1CC4L, 0xCE6C1CC4L);
+	return 0;
+}
