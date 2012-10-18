7378776613e524a6e9b4f41793c198a8a01b2992
Sebastian Buchwald <Sebastian.Buchwald@kit.edu>
2012-10-18 18:47:52 +0200

    Fixed test for 64 Bit systems.

--- C/should_fail/large_array.c
+++ C/should_fail/large_array.c
@@ -1,9 +1,10 @@
 /* strictly the behaviour of this is implementation defined I think, but every
- * sane compiler should report an error here */
-int a[2147483647][2147483647][2147483647];
+ * sane compiler should report an error here
+ * 3 * 7 * 29 * 36760123 * 823996703 = 2^64 + 5 */
+int a[609][36760123][823996703];
 
 int main(void)
 {
-	a[2147483646][2147483646][2147483646] = 0;
-	return a[2147483646][2147483646][2147483646] = 0;
+	a[608][36760122][823996702] = 0;
+	return a[608][36760122][823996702] = 0;
 }
