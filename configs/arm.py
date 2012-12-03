1b9b5381696369b0dbc852eaaf539022a0c480bd
Manuel Mohr <manuel.mohr@kit.edu>
2012-12-03 17:29:08 +0100

    Update ARM plugin.

--- configs/arm.py
+++ configs/arm.py
@@ -2,9 +2,9 @@ def setup_arm(option, opt_str, value, parser):
 	global _ARCH_DIRS
 	_ARCH_DIRS = [ "armcode" ]
 	config = parser.values
-	config.cflags += " -mtarget=arm-linux-gnu"
+	config.cflags += " -mtarget=arm-linux-gnueabihf"
 	config.ldflags += " -static"
 	config.runexe = "qemu-arm "
-	config.expect_url = "fail_expectations_arm"
+	config.expect_url = "http://pp.info.uni-karlsruhe.de/git/firm-testresults/plain/fail_expectations-arm-linux-gnueabihf"
 
 configurations['arm'] = setup_arm
