c41cd7ff6718b4353d887a40eb860bd97eeb78ef
Matthias Braun <matze@braunis.de>
2012-12-05 19:37:38 +0100

    add musl libc testcase
    Currently libfirm only support "should_be_same" constraints, which are
    always enough for our internal nodes. For this asm we need a guaranteed
    "must_be_same"...

---
+++ x86code/asm_test13.c
@@ -0,0 +1,13 @@
+/* as seen in musl libc */
+static inline int a_swap(volatile int *x, int v)
+{
+	__asm__( "xchg %0, %1" : "=r"(v), "=m"(*x) : "0"(v) : "memory" );
+	return v;
+}
+
+int main(void)
+{
+	static int lock;
+	while (a_swap(&lock, 1));
+	return 0;
+}
