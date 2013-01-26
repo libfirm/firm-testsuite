0168da2c26284f6eb33bf9f0b774ad8e05e70640
Matthias Braun <matze@braunis.de>
2013-01-26 16:32:29 +0100

    test for builtin dynamic preprocessor macros

---
+++ preproctest/builtins.c
@@ -0,0 +1,15 @@
+File: __FILE__
+Line: __LINE__
+Counter: __COUNTER__
+IncludeLevel: __INCLUDE_LEVEL__
+Basefile: __BASE_FILE__
+/* can't unit-test __DATE__, __TIME__, __TIMESTAMP__ in a robust manner with
+ * predictable output */
+go down
+#include "builtins.h"
+back up
+File: __FILE__
+Line: __LINE__
+Counter: __COUNTER__
+IncludeLevel: __INCLUDE_LEVEL__
+Basefile: __BASE_FILE__
---
+++ preproctest/builtins.h
@@ -0,0 +1,5 @@
+File: __FILE__
+Line: __LINE__
+Counter: __COUNTER__
+IncludeLevel: __INCLUDE_LEVEL__
+Basefile: __BASE_FILE__
