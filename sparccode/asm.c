17c49d437433bc0eeac086518386dce9dce8a63a
Matthias Braun <matze@braunis.de>
2013-04-09 18:59:22 +0200

    extend (and fix) sparc asm test

--- sparccode/asm.c
+++ sparccode/asm.c
@@ -11,7 +11,7 @@ static inline unsigned cas(unsigned *addr, unsigned oldval, unsigned newval)
 {
 	unsigned result;
 #ifdef __leon__
-	__asm__ __volatile__("cas [%[address]], %[new], %[old]"
+	__asm__ __volatile__("cas [%[address]], %[old], %[new]"
 		: "=r" (result)
 		: [new] "0"  (newval), [address] "r"  (addr), [old] "r"  (oldval)
 		: "memory");
@@ -24,7 +24,27 @@ static inline unsigned cas(unsigned *addr, unsigned oldval, unsigned newval)
 	return result;
 }
 
+static inline unsigned cas2(unsigned *addr, unsigned oldval, unsigned newval)
+{
+#ifdef __leon__
+	__asm__ __volatile__("stbar\n\tcas [%[address]], %[old], %[new]"
+		: [new] "+r" (newval)
+		: [address] "r"  (addr), [old] "r"  (oldval)
+		: "memory");
+	return newval;
+#else
+	// not atomic... but this file is just a stupid compilertest
+	unsigned result = *addr;
+	if (result == oldval)
+		*addr = newval;
+	return result;
+#endif
+}
+
+
+
 static unsigned a0;
+static unsigned a1;
 unsigned g0 = 13;
 unsigned g1 = 66;
 
@@ -35,6 +55,12 @@ int main(void)
 	unsigned r1 = cas(&a0, 42, 3);
 	unsigned r2 = cas(&a0, 42, 7);
 	printf("%u %u %u %u\n", a0, r0, r1, r2);
+
+	a1 = 17;
+	unsigned c0 = cas2(&a1, 17, 88);
+	unsigned c1 = cas2(&a1, 42, 3);
+	unsigned c2 = cas2(&a1, 42, 7);
+	printf("%u %u %u %u\n", a1, r0, r1, r2);
 
 	unsigned add1 = add(42, 7);
 	unsigned add2 = add(g0, g1);
