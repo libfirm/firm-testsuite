dd68232d29e4ef6cd3c08f5211b79d787b232975
Sebastian Buchwald <Sebastian.Buchwald@kit.edu>
2013-05-22 20:20:33 +0200

    Added another test case for occult constants.

---
+++ opt/occult3.c
@@ -0,0 +1,21 @@
+int a;
+int b;
+int c;
+
+short fn1(void)
+{
+	return c;
+}
+
+int main(void)
+{
+	int d = 0;
+	int *e = &d;
+
+	for (b = 0; b < 1; b++) {
+	}
+
+	a = *e > 0 >= 0 >= fn1();
+
+	return 0;
+}
