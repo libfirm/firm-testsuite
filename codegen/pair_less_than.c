29db84a468d0db24e03bb1bb06acc95f1577e6e7
Matthias Braun <matze@braunis.de>
2013-08-15 04:52:10 +0200

    some interesting codegen challenge for cparser/clang

---
+++ codegen/pair_less_than.c
@@ -0,0 +1,21 @@
+#include <stdbool.h>
+
+/* the following 2 functions are a typical implementation of a "less than"
+ * operator comparing two pairs.
+ * Semantically both versions perform the same operation, however the generated
+ * code differs quiet a bit between compilers.
+ */
+
+bool lessthan1(unsigned lx, unsigned rx, unsigned ly, unsigned ry) {
+  if (lx != rx)
+    return lx < rx;
+  return ly < ry;
+}
+
+bool lessthan2(unsigned lx, unsigned rx, unsigned ly, unsigned ry) {
+  if (lx < rx)
+    return true;
+  if (lx > rx)
+    return false;
+  return ly < ry;
+}
