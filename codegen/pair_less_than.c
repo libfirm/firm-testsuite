df70070a6f131578c2eb8cdb81da068007c35725
Christoph Mallon <christoph.mallon@gmx.de>
2013-08-27 11:00:19 +0200

    Fix typo in comment.

--- codegen/pair_less_than.c
+++ codegen/pair_less_than.c
@@ -3,7 +3,7 @@
 /* the following 2 functions are a typical implementation of a "less than"
  * operator comparing two pairs.
  * Semantically both versions perform the same operation, however the generated
- * code differs quiet a bit between compilers.
+ * code differs quite a bit between compilers.
  */
 
 bool lessthan1(unsigned lx, unsigned rx, unsigned ly, unsigned ry) {
