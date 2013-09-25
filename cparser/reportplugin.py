a83c395a1efcc0dfc4bb7ceac714e383870c3d49
Matthias Braun <matze@braunis.de>
2013-09-25 10:06:48 -0700

    make badopt4.cmd a should_warn

--- cparser/reportplugin.py
+++ cparser/reportplugin.py
@@ -1,7 +1,7 @@
 from plugins.c   import setup_c_environment
 from test.test   import Test
 from test.steps  import execute
-from test.checks import check_retcode_zero
+from test.checks import check_retcode_zero, check_no_errors, create_check_warnings_reference
 import os.path
 
 def step_runcommand(environment):
@@ -26,20 +26,31 @@ def make_cmd_should_fail(environment, filename):
     read_cmds(environment, filename)
 
     test = Test(environment, filename)
-    preprocess = test.add_step("cmd_should_fail", step_runcommand)
-    preprocess.add_check(check_retcode_not_zero)
+    runcompiler = test.add_step("cmd_should_fail", step_runcommand)
+    runcompiler.add_check(check_retcode_not_zero)
     return test
+
+def make_cmd_should_warn(environment, filename):
+	read_cmds(environment, filename)
+
+	test = Test(environment, filename)
+	runcompiler = test.add_step("cmd_should_warn", step_runcommand)
+	runcompiler.add_check(check_retcode_zero)
+	runcompiler.add_check(check_no_errors)
+	runcompiler.add_check(create_check_warnings_reference(environment))
+	return test
 
 def make_cmd_should_work(environment, filename):
     read_cmds(environment, filename)
 
     test = Test(environment, filename)
-    preprocess = test.add_step("cmd_should_work", step_runcommand)
-    preprocess.add_check(check_retcode_zero)
+    runcompiler = test.add_step("cmd_should_work", step_runcommand)
+    runcompiler.add_check(check_retcode_zero)
     return test
 
 test_factories = [
     (lambda name: name.endswith(".cmd") and "should_fail/" in name, make_cmd_should_fail),
+	(lambda name: name.endswith(".cmd") and "should_warn/" in name, make_cmd_should_warn),
     (lambda name: name.endswith(".cmd")                           , make_cmd_should_work),
 ]
 wildcard_factories = [
