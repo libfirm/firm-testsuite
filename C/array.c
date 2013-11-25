d3458c3a6cd97b42a30e31f8804aee2c7dfbdb0d
Matthias Braun <matze@braunis.de>
2013-11-25 18:56:48 +0100

    fix testcase for non 32bit archs

--- C/array.c
+++ C/array.c
@@ -1,11 +1,14 @@
+#include <assert.h>
+
 int printf(const char *str, ...);
 
 int main(void)
 {
 	int array[4];
 	unsigned st = sizeof(array + 1);
-	printf("Res: %d %d %d %d (should be 4, 16, 16, 16 if sizeof(int)==4)\n",
-	       (int) sizeof(array+1), (int) sizeof(array),
-	       (int) sizeof(*&array), (int) sizeof( (array) ));
+	assert(sizeof(array+1) == sizeof(void*));
+	assert(sizeof(array) == sizeof(int)*4);
+	assert(sizeof(*&array) == sizeof(int)*4);
+	assert(sizeof( (array) ) == sizeof(int)*4);
 	return 0;
 }
