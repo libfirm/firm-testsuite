daabd551a58a6d835fe6f272ccd8990860b5f856
Matthias Braun <matze@braunis.de>
2013-11-25 17:24:56 +0100

    improve import/export tester

--- scripts/cparser_imexport.py
+++ scripts/cparser_imexport.py
@@ -1,35 +1,63 @@
 #!/usr/bin/env python
+#
+# Stupid cparser wrapper which first exports the C file to a .ir file,
+# and then imports this .ir file and compiles it.
+# This way you can test the .ir import/export facilities.
 import sys
 import os
-from subprocess import check_output
+from subprocess import check_output, CalledProcessError
 
-newargs = [ "cparser" ]
-# Find -c and -o argument
-i = 1
-irfile = None
-while i < len(sys.argv):
-	arg = sys.argv[i]
-	if arg == "-o":
+debug = "CPARSER_IMEXPORT_DEBUG" in os.environ
+
+def exec_cparser(args):
+	cmdline = ["cparser"] + args
+	if debug:
+		print " ".join(cmdline)
+	try:
+		out = check_output(cmdline)
+		sys.stdout.write(out)
+	except CalledProcessError as e:
+		# try to convert python style negative returncodes on signals back to
+		# linuxish returncodes (128 + signal number)
+		if e.returncode < 0:
+			e.returncode = 128 + -(e.returncode)
+		sys.exit(e.returncode)
+
+def go(argv):
+	# Don't do anything for -fsyntax-only or --E
+	if ("-fsyntax-only" in argv) or ("-E" in argv):
+		exec_cparser(argv[1:])
+		return
+
+	# Step 1 --export-ir
+	newargs = []
+	# Find -c and -o argument
+	i = 1
+	irfile = None
+	while i < len(argv):
+		arg = argv[i]
+		if arg == "-o":
+			i += 1
+			arg = argv[i]
+			irfile = arg.replace(".exe", "") + ".ir"
+			newargs.append("--export-ir")
+			newargs.append("-o")
+			newargs.append(irfile)
+		else:
+			newargs.append(arg)
 		i+=1
-		arg = sys.argv[i]
-		irfile = arg.replace(".exe", "") + ".ir"
-		newargs.append("--export-ir")
-		newargs.append("-o")
-		newargs.append(irfile)
-	else:
+
+	exec_cparser(newargs)
+
+	# Step 2 compile .ir file
+	newargs = []
+	for arg in argv[1:]:
+		if arg.endswith(".c"):
+			newargs.append(irfile)
+			continue
 		newargs.append(arg)
-	i+=1
 
-check_output(newargs)
+	exec_cparser(newargs)
 
-newargs = [ "cparser" ]
-i = 1
-while i < len(sys.argv):
-	arg = sys.argv[i]
-	if arg.endswith(".c"):
-		newargs.append(irfile)
-	else:
-		newargs.append(arg)
-	i+=1
-
-check_output(newargs)
+if __name__ == "__main__":
+	go(sys.argv)
