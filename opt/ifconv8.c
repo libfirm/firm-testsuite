1dcaef3020c26c5f80f0e09f7bf4139d4ae6550a
Sebastian Buchwald <Sebastian.Buchwald@kit.edu>
2013-11-28 16:22:37 +0100

    Added another crashing if-conversion test.

---
+++ opt/ifconv8.c
@@ -0,0 +1,21 @@
+int a;
+int b;
+int c;
+int d;
+int e;
+long long f;
+
+int main(void)
+{
+	for (;;) {
+		int g = 0;
+		for (; a;)
+			g = b == 0;
+		if (!c)
+			break;
+		d = g < 1;
+		e = d == f;
+	}
+
+	return 0;
+}
