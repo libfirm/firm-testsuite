f189fe64c450f455f3f30aae03f47d98638dfd33
Sebastian Buchwald <Sebastian.Buchwald@kit.edu>
2013-12-04 09:28:51 +0100

    Added test that crashes during register allocation.

---
+++ backend/chordal.c
@@ -0,0 +1,16 @@
+int a;
+int b;
+long long c;
+int d;
+int e;
+int f;
+int g;
+
+int main(void)
+{
+	int h = a + b;
+	c = c > 0 ? 0 : c + 1;
+	d = e / (unsigned)(((f ? 0 : g) == h && a) < 2 ^ 5926);
+
+	return 0;
+}
