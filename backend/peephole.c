55a10607e5c77753c295435f86b97fe3e8f04914
Sebastian Buchwald <Sebastian.Buchwald@kit.edu>
2013-12-05 13:08:24 +0100

    Extended test.
    We can learn some code generation tricks from gcc here.

--- backend/peephole.c
+++ backend/peephole.c
@@ -1,5 +1,12 @@
 int a = -1L;
-int main()
+int b = -1L;
+int c = -1L;
+int d = -1L;
+
+int main(void)
 {
-	return (a & 128) <= 0;
+	return ((a & (int)0x00000080)) <= 0 |
+	       ((b & (int)0x00008000)) <= 0 |
+	       ((c & (int)0x00800000)) <= 0 |
+	       ((d & (int)0x80000000)) >  0;
 }
