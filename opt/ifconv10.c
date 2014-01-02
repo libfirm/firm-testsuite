c7cbacac959ebf31633b4e54605f775581a8a999
Sebastian Buchwald <Sebastian.Buchwald@kit.edu>
2014-01-02 09:36:02 +0100

    If-conversion produces invalid phi nodes.

---
+++ opt/ifconv10.c
@@ -0,0 +1,24 @@
+int a;
+int b;
+int c;
+
+int fn1(long long p1)
+{
+	return 0;
+}
+
+void fn2(void) {
+	for (;;) {
+		int d = 0;
+		c && (d = a != 0);
+		if (c)
+			b = fn1(d >= 1);
+		else
+			return;
+	}
+}
+
+int main(void)
+{
+	return 0;
+}
