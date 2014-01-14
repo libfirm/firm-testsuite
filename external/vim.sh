b01c8caa65869846f7a8a2d52b9928f9546cbf70
Matthias Braun <matze@braunis.de>
2014-01-14 14:55:35 +0100

    fix vim external build script on x86_64 computers

--- external/vim.sh
+++ external/vim.sh
@@ -13,5 +13,5 @@ cd "$BUILDDIR"
 echo "Extracting..."
 tar -xf "$PACKAGEDIR/vim-$VIMVERSION.tar.bz2"
 cd "vim$VIMVERSIONSHORT"
-CC=cparser CFLAGS="-O2 -m32" ./configure --prefix=/tmp/vimtest --disable-gtk-check --disable-gtk2-check --disable-gnome-check --disable-motif-check --disable-athena-check --disable-nextaw-check --disable-carbon-check --disable-gpm --disable-nls --disable-acl
+CC=cparser CFLAGS="-O2 -m32" LDFLAGS="-m32" ./configure --prefix=/tmp/vimtest --disable-gtk-check --disable-gtk2-check --disable-gnome-check --disable-motif-check --disable-athena-check --disable-nextaw-check --disable-carbon-check --disable-gpm --disable-nls --disable-acl
 make $MAKEFLAGS
