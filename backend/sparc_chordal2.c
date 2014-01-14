c922fb3de03f6d8982c696ba05f5dec6594c36aa
Sebastian Buchwald <Sebastian.Buchwald@kit.edu>
2014-01-14 14:14:55 +0100

    Added test that triggers assertion during chordal coloring.

---
+++ backend/sparc_chordal2.c
@@ -0,0 +1,27 @@
+int a, b, c, d, f, g, h, i, j, k;
+long long e;
+
+void fn1(int p1) {
+	int l     = 0;
+	int m[16] = {};
+	for (;;) {
+		for (k = 0; k <= 0; k++) {
+			h = l < 0 <= c ^ b;
+			g = h;
+			e = !g == 5;
+			j = e;
+			i = m[0] || p1;
+		}
+		f = e--;
+	}
+}
+
+void fn2(void)
+{
+	fn1(a ^ d);
+}
+
+int main(void)
+{
+	return 0;
+}
