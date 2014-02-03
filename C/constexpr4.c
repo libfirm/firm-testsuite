4e6b404f77b8053d019a0cb3ebb9c0831449b686
Tobias Rapp <tobias.rapp@student.kit.edu>
2014-02-03 14:22:04 +0100

    the expression in constexpr4 is only constant if pointers and int have the same size

--- C/constexpr4.c
+++ C/constexpr4.c
@@ -1,8 +1,13 @@
+#include <stdint.h>
+#include <limits.h>
+
+#if (INTPTR_MAX == INT_MAX) && (INTPTR_MIN == INT_MIN)
 int qftol(void) {
 	return 0;
 }
 
 static int ftolPtr = (int) qftol;
+#endif
 
 int main(void) {
 	return 0;
