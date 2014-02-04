94b368f427e93fcf182c02365939474470600ee8
Christoph Mallon <christoph.mallon@gmx.de>
2014-02-04 11:01:27 +0100

    Use intptr_t instead of int to ensure that the casted address is a linker constant.

--- C/constexpr4.c
+++ C/constexpr4.c
@@ -1,13 +1,10 @@
 #include <stdint.h>
-#include <limits.h>
 
-#if (INTPTR_MAX == INT_MAX) && (INTPTR_MIN == INT_MIN)
 int qftol(void) {
 	return 0;
 }
 
-static int ftolPtr = (int) qftol;
-#endif
+static intptr_t ftolPtr = (intptr_t)qftol;
 
 int main(void) {
 	return 0;
