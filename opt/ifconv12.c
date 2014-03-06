437fcdcfa1be22d5034b3070af5edff8b0995b7d
Sebastian Buchwald <Sebastian.Buchwald@kit.edu>
2014-03-06 10:14:04 +0100

    Added if-conversion test that results in an output mismatch.

---
+++ opt/ifconv12.c
@@ -0,0 +1,20 @@
+int a;
+long long b;
+
+int fn1(void)
+{
+	return 0;
+}
+
+int main(void)
+{
+	for (int c = 2; c; c--) {
+		int d = a != 1;
+		int e = d != 1;
+		if (fn1())
+			break;
+		b = e >= d;
+	}
+
+	return b;
+}
