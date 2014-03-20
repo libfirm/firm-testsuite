8fde7dd5d09626c3fe64ff64211eaa6012b75169
Matthias Braun <matze@braunis.de>
2014-03-20 13:35:27 +0100

    we should not warn when compating pointers to compatible types with different qualifiers

--- C/nowarn/compare.c
+++ C/nowarn/compare.c
@@ -7,6 +7,8 @@ int main(void)
 	(void) (a == (void*)0);
 	(void) (a == 0);
 	(void) (a == (void*)42);
+	(void) (a == (const struct A*)a);
+	(void) (a == (const volatile struct A*)a);
 
 	return 0;
 }
