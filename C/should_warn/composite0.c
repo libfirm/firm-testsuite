6172d422da2cc0a12ef459a122e3ae9e276d1c57
Matthias Braun <matze@braunis.de>
2014-03-20 15:22:30 +0100

    test if composite types are constructed

---
+++ C/should_warn/composite0.c
@@ -0,0 +1,13 @@
+/*$ -Wno-redundant-decls -Wno-strict-prototypes $*/
+extern void f(void(*)(),       void(*)(int));
+extern void f(void(*)(double), void(*)());
+
+extern void g(double a);
+extern void h(int a);
+struct S { int a, b, c; };
+extern void oops(const struct S s);
+
+int main(void)
+{
+	f(g, oops);
+}
