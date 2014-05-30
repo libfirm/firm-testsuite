ae4c05c94ff9ef820aab2f125b080cb3a1514ba5
Matthias Braun <matze@braunis.de>
2014-05-30 18:17:13 +0200

    move arm fail_expectations back

--- configs/arm.py
+++ configs/arm.py
@@ -4,7 +4,7 @@ def config_arm(option, opt_str, value, parser):
     config.arch_cflags  = "-mtarget=arm-linux-gnueabi"
     config.arch_ldflags = "-static"
     config.runexe       = "qemu-arm "
-    config.expect_url   = "http://pp.info.uni-karlsruhe.de/git/firm-testresults/plain/fail_expectations-arm-linux-gnueabi"
+    config.expect_url   = "fail_expectations-arm-linux-gnueabi"
 
 configurations = {
     'arm': config_arm
