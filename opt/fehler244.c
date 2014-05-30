30ff77bb5a56c3b288993c68b500f4adcd37bb46
Sebastian Buchwald <Sebastian.Buchwald@kit.edu>
2014-05-30 11:18:41 +0200

    Added test that results in an output mismatch.

---
+++ opt/fehler244.c
@@ -0,0 +1,26 @@
+int a;
+int b;
+char c = 1;
+int d;
+int e;
+
+char fn1(char p1)
+{
+	return p1 == 0 ? 0 : 0 / p1;
+}
+
+int main(void)
+{
+	int *f = &a;
+	int **g[] = { &f };
+	*f = 0;
+	b = 0;
+	c |= a;
+	int h = *f;
+	d = a;
+	*f = fn1(h);
+	for (; e < 1; e++)
+		return c - 1;
+
+	return 0;
+}
