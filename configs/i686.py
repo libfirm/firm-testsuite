656a5ec9d58925202cfca4a449795d9367d1df81
Matthias Braun <matze@braunis.de>
2014-06-03 16:22:52 +0200

    improve i686 config

--- configs/i686.py
+++ configs/i686.py
@@ -2,7 +2,9 @@ target_triple = "i686-linux-gnu"
 def config_i686(option, opt_str, value, parser):
     config = parser.values
     config.arch_dirs    = []
-    config.arch_cflags  = "-mtarget=" + target_triple + " -march=native -m32"
+    # Note that we do leave out target, so cparser does not think we are
+    # cross-compiling...
+    config.arch_cflags  = "-march=native -m32"
     config.arch_ldflags = "-m32"
     config.expect_url   = "http://pp.info.uni-karlsruhe.de/git/firm-testresults/plain/fail_expectations-" + target_triple
 
