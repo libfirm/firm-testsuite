8a4bd9540d1cb382c30f994d494a8c72167baf42
Sebastian Buchwald <Sebastian.Buchwald@kit.edu>
2014-09-09 17:11:42 +0200

    SPARC: Generated binary segfaults during execution.

---
+++ backend/sparc_segfault.c
@@ -0,0 +1,67 @@
+int a, b, c, d, e, f, p, x, t5, t6, t8;
+unsigned g;
+struct S0 {
+	int f0;
+} t14;
+short h, y, t1;
+int *k = &e, *w = &e, *t16, *t18, *t13 = &a;
+char l, n, q, r, s;
+long long m, t3;
+short *v = &h;
+char *t2;
+unsigned long long *t4;
+int **t12 = &k;
+
+int fn1(int p1)
+{
+	return a;
+}
+
+int fn2(void)
+{
+	return a;
+}
+
+int fn3(int p1)
+{
+	return p1 && a ? 0 : a;
+}
+
+int fn5(int p1)
+{
+	return h;
+}
+
+struct S0 fn7(char p1, int p2)
+{
+	for (;;) {
+		int *t22 = &e;
+		int **t23[30] = { &t22 };
+		t6 = fn1(7 > (*t13 = e) | 0 == *t22 &(m = b) != 0);
+		g = t5 <= 9 >= *k;
+		x = fn1(e);
+		if (a) {
+			*w = t6;
+			*t16 = **t12 ^= h | 0 != t22;
+			*k &= p1 && l != t6 == p1 & p1;
+			t8 = fn1(*t18);
+		} else
+			return t14;
+	}
+}
+
+int main(void)
+{
+	int t9 = 0;
+	g = 0;
+	fn7(a, (*w = s == (*v = h >= fn5(a = (0 || t5) > fn5(r))) & r == b, c));
+	if (a < q) {
+		int t10[28] = { e };
+		*v = x = *w = m || 0;
+		t6 = 0;
+		fn3(1 && l >= 6 & t8);
+		t9 = 0 != fn2() == (y = f) > (*t4 = *t2 = fn1(t1) != t3 || p < n == d) == n;
+		int *t11[100] = { &t9 };
+	}
+	return 0;
+}
