8779b9569bf75aaab6e41bba4d25db0450fcceaa
Sebastian Buchwald <Sebastian.Buchwald@kit.edu>
2014-10-09 10:30:28 +0200

    Verifier complains about unscheduled nodes.

---
+++ backend/scheduled.c
@@ -0,0 +1,28 @@
+int a;
+int b;
+int c;
+int *d;
+volatile int e;
+
+int fn1(int p1)
+{
+	return 0;
+}
+
+void fn2(void)
+{
+	int g;
+	for (; b; --b) {
+		int *h = 0;
+		int f = fn1(c);
+		*h &= f;
+		e;
+		*h &= *d = fn1(a);
+	}
+	int *i[930] = { &g };
+}
+
+int main(void)
+{
+	return 0;
+}
