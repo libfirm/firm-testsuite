8c502473d6f95f9e5827d2f6d60bb6f4abc96998
Matthias Braun <matze@braunis.de>
2014-12-25 13:55:47 -0800

    non-elf system do not support alias

--- C/gnu99/cp_error078.c
+++ C/gnu99/cp_error078.c
@@ -2,7 +2,11 @@ static void default_foo_implementation(void)
 {
 }
 
+#ifdef __ELF__
 extern __typeof(default_foo_implementation) foo_alias __attribute__((weak, alias("default_foo_implementation")));
+#else
+#define foo_alias default_foo_implementation
+#endif
 
 int main(void)
 {
