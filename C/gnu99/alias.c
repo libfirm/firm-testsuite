9f7d1407cc33e53561ea1427a15b5071e38d9643
Matthias Braun <matze@braunis.de>
2014-12-25 13:26:19 -0800

    non-elf systems do not support hard aliases

--- C/gnu99/alias.c
+++ C/gnu99/alias.c
@@ -1,9 +1,18 @@
+#ifdef __ELF__
 int __attribute__((alias("puts"))) put_string(const char *str);
 extern int puts(const char *str);
 int __attribute__((alias("puts"))) PutString(const char *str);
 
 int var = 42;
 extern int __attribute__((alias("var"))) value;
+#else
+// Target does not support such alias usage
+#define put_string puts
+#define PutString puts
+#define var value
+int var = 42;
+extern int puts(const char *str);
+#endif
 
 int main(void)
 {
--- C/gnu99/alias3.c
+++ C/gnu99/alias3.c
@@ -1,7 +1,13 @@
 extern int puts(const char *str);
+
+#ifdef __ELF__
 int put_string(const char *str) __attribute__((alias("puts")));
 
 int PutString(const char *str);
+#else
+#define put_string puts
+#define PutString puts
+#endif
 
 int main(void)
 {
@@ -10,4 +16,6 @@ int main(void)
 	return 0;
 }
 
+#ifdef __ELF__
 int __attribute__((alias("puts"))) PutString(const char *);
+#endif
