73db55a193c6771542e335e35fe26f31ecfc7b15
Christoph Mallon <mallon@cs.uni-saarland.de>
2015-07-10 08:51:50 +0200

    Make test case work without wprintf().
    Its main point is wide string concatenation, not wprintf().

--- C/doublewide.c
+++ C/doublewide.c
@@ -1,6 +1,10 @@
+#include <stdio.h>
 #include <wchar.h>
 
 int main(void) {
-	wprintf(L"Ha" L"llo");
+	wchar_t const* s = L"Ha" L"llo\n";
+	while (*s != L'\0') {
+		putchar(*s++);
+	}
 	return 0;
 }
