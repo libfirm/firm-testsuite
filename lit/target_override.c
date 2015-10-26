ffe8527f7a0520f16ccb79abefc9e79bcb75335e
Matthias Braun <matze@braunis.de>
2015-10-26 00:21:57 -0700

    add some tests for cparser target initialisation

---
+++ lit/target_override.c
@@ -0,0 +1,9 @@
+// RUN: cparser -integrated-cpp -E -dM -x c /dev/null -fshort-wchar -target i686-linux-gnu | LC_ALL=C sort | FileCheck %s --check-prefix SHORT_WCHAR
+// RUN: cparser -integrated-cpp -E -dM -x c /dev/null -target i686-linux-gnu -fshort-wchar | LC_ALL=C sort | FileCheck %s --check-prefix SHORT_WCHAR
+// RUN: cparser -integrated-cpp -E -dM -x c /dev/null -target i686-linux-gnu | LC_ALL=C sort | FileCheck %s --check-prefix INT_WCHAR
+// RUN: cparser -integrated-cpp -E -dM -x c /dev/null -fshort-wchar -target i686-linux-gnu -fno-short-wchar | LC_ALL=C sort | FileCheck %s --check-prefix INT_WCHAR
+
+// Tests that specific options like -fshort-wchar override target defaults set
+// by -target.
+// SHORT_WCHAR: #define __SIZEOF_WCHAR_T__ 2
+// INT_WCHAR: #define __SIZEOF_WCHAR_T__ 4
