0e599a7db113ed9b32eb52896473d2edb5886e9b
Matthias Braun <matze@braunis.de>
2015-12-06 11:36:16 +0100

    improve firm-21.sh

--- external/firm-1.21.sh
+++ external/firm-1.21.sh
@@ -1,26 +1,27 @@
 #!/bin/sh
 #
 # Test libfirm (bootstrap)
-set -e
+set -eu
 
 . "$(dirname "$0")/utils.sh"
 
 FIRMVERSION="1.21.0"
-grab "http://switch.dl.sourceforge.net/sourceforge/libfirm/libfirm-$FIRMVERSION.tar.bz2"
+grab "http://switch.dl.sourceforge.net/sourceforge/libfirm/libfirm-$FIRMVERSION.tar.bz2" "TODO:MD5"
 CPARSERVERSION="0.9.14"
-grab "http://switch.dl.sourceforge.net/sourceforge/cparser/cparser-$CPARSERVERSION.tar.bz2"
+grab "http://switch.dl.sourceforge.net/sourceforge/cparser/cparser-$CPARSERVERSION.tar.bz2" "TODO:MD5"
 cd "$BUILDDIR"
 echo "Extracting..."
 tar -xf "$PACKAGEDIR/libfirm-$FIRMVERSION.tar.bz2"
 tar -xf "$PACKAGEDIR/cparser-$CPARSERVERSION.tar.bz2"
 cd "libfirm-$FIRMVERSION"
+patch -p1 < "$TOPDIR/libfirm-$FIRMVERSION.patch"
 TEMPINSTALLPATH="/tmp/firmtest"
 rm -rf "$TEMPINSTALLPATH"
 CC=cparser LFLAGS="-m32" CFLAGS="-O2 -m32" ./configure "--prefix=$TEMPINSTALLPATH"
 make $MAKEFLAGS
 make install
 cd "../cparser-$CPARSERVERSION"
-sed -e 's/-O0/-O1/g' -i Makefile # cparser fails on -O0 :-(
+sed -e 's/-O0/-O2/g' -i Makefile # cparser fails on -O0 :-(
 export PKG_CONFIG_PATH="$TEMPINSTALLPATH/lib/pkgconfig"
 export LD_LIBRARY_PATH="$TEMPINSTALLPATH/lib:$LD_LIBRARY_PATH"
 CC=cparser LFLAGS="-m32" CPPFLAGS="-m32" CFLAGS="-O2 -m32" make $MAKEFLAGS bootstrap2
