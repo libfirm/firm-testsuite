e7a5a2df4522451b06cc651d23e65472b2dfa1c6
Sebastian Buchwald <Sebastian.Buchwald@kit.edu>
2015-12-21 15:38:10 +0100

    Spilling a phi triggers an assertion in optimize_node.

---
+++ backend/spill_phi.c
@@ -0,0 +1,32 @@
+int a, b, d, e, f, g, h, i, j, k, l, m, n, o;
+char c[1];
+
+void fn1(int p1)
+{
+	b || p1 ? 0 : 0 / 1;
+}
+
+int fn2(long long p1, unsigned p2)
+{
+	fn1(i | (p2 || e));
+	for (; m;) {
+		j |= n;
+		for (; k;) {
+			l = 9;
+		}
+		o = a + (p1 >= 82532920964170);
+	}
+	return 0;
+}
+
+void fn3(char p)
+{
+	for (;;) {
+		h = f = g = fn2(c[d], p < 0);
+	}
+}
+
+int main(void)
+{
+	return 0;
+}
