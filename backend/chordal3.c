8d582632d53babf77c805e82d95a1e6cf4b257c7
Sebastian Buchwald <Sebastian.Buchwald@kit.edu>
2016-01-15 23:04:07 +0100

    Simplify test.

--- backend/chordal3.c
+++ backend/chordal3.c
@@ -1,19 +1,12 @@
-void fn1(void *p)
-{
-}
-
-struct s1 {
-	void (*f)(void *);
-} a = { &fn1 };
-
-void *__attribute__((noinline)) fn2(void)
-{
-	return &a;
-}
+struct {
+	void (*f)(int *);
+} *a;
 
 int main(void)
 {
-	struct s1 *a = fn2();
-	a->f(a);
+	if (a) {
+		a->f(a);
+	}
+
 	return 0;
 }
