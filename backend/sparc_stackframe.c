67f9bafb9d7c8a959d8b3bda18c8b1740b19eb43
Sebastian Buchwald <Sebastian.Buchwald@kit.edu>
2016-01-15 19:46:26 +0100

    Slightly simplify test.

--- backend/sparc_stackframe.c
+++ backend/sparc_stackframe.c
@@ -2,9 +2,9 @@ struct S1 {
 	int *f1;
 } a;
 
-struct S1 fn1(int p1, long long p2, int p3, int p4, int p5)
+struct S1 fn1(int p1, int p2, int p3, int p4, int p5, int p6)
 {
-	a.f1 = &p5;
+	a.f1 = &p6;
 	return a;
 }
 
