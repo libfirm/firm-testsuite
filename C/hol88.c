d508e2945e1a05ae6ed39e0edb528054dba0d1ca
Andreas Seltenreich <seltenreich@gmx.de>
2016-01-23 21:55:11 +0100

    Failing assertion on amd64, reduced from HOL88's mk-hol-lcf.c.

---
+++ C/hol88.c
@@ -0,0 +1,12 @@
+/* reduced from HOL88's mk-hol-lcf.c */
+/* ir/tv/tv.c:1105: tarval_shr_unsigned: Assertion `mode_is_int_(mode)' failed. */
+
+int a;
+void fn1() {
+  ({
+    long b = (long)&a - (0x8000000000000000 + (4611686018427387904 >> 1));
+    b + (4611686018427387904 >> 1) ?: 0;
+  });
+}
+
+int main(void) { return 0; }
