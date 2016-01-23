3693ce6c71be6d18a0da06449a96c568601da534
Andreas Seltenreich <seltenreich@gmx.de>
2016-01-23 23:18:51 +0100

    Add testcase for failed assertion on -ffast-math.

---
+++ C/xaos-formulas.c
@@ -0,0 +1,17 @@
+/*$ -ffast-math $*/
+/* reduced from xaos-3.5+ds1/src/engine/formulas.c */
+
+int a;
+double b, c, d;
+unsigned fn1() {
+  goto periodicity;
+  do
+    c = b + b;
+  while (d);
+periodicity:
+  return a;
+}
+
+int main(void) {
+  return 0;
+}
