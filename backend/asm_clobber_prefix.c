45ecb4645961b184febd0e13f9fecffa912b546c
Christoph Mallon <christoph.mallon@gmx.de>
2016-02-15 18:32:01 +0100

    Add test case for asm clobber with register prefix.

---
+++ backend/asm_clobber_prefix.c
@@ -0,0 +1,29 @@
+#if defined __amd64__
+#define RESULT "rax"
+#elif defined __arm__
+#define RESULT "r0"
+#elif defined __i386__
+#define RESULT "eax"
+#elif defined __mips__
+#define RESULT "v0"
+#define PREFIX "$"
+#elif defined __sparc__
+#define RESULT "o0"
+#else
+#error unknown architecture
+#endif
+
+int __attribute__((noinline)) zero(void) { return 0; }
+
+int main(void)
+{
+	int x = zero();
+	/* GCC accepts these two for all architectures.
+	 * See strip_reg_name() in varasm.c */
+	asm("/* %% */" ::: "%" RESULT);
+	asm("/* # */" ::: "#" RESULT);
+#ifdef PREFIX
+	asm("/* " PREFIX " */" ::: PREFIX RESULT);
+#endif
+	return x;
+}
