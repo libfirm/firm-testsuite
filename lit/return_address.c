98adb4f3262e8a7b170b9e580b845a371b3fe304
Matthias Braun <matze@braunis.de>
2016-02-14 17:16:16 -0800

    Add test for builtin_return_address

---
+++ lit/return_address.c
@@ -0,0 +1,15 @@
+// RUN: {cc} -integrated-cpp -target i686-unknown-linux-gnu -O3 -m32 -fomit-frame-pointer -S -o- {input} | FileCheck {input} --check-prefix CHECK --check-prefix IA32-OMITFP
+// RUN: {cc} -integrated-cpp -target i686-unknown-linux-gnu -O3 -m32 -fno-omit-frame-pointer -S -o- {input} | FileCheck {input} --check-prefix CHECK --check-prefix IA32-NOOMITFP
+
+// CHECK-LABEL: retaddr:
+void *retaddr(void)
+{
+	// IA32-NOOMITFP: pushl %ebp
+	// IA32-NOOMITFP: movl %esp, %ebp
+	// IA32-NOOMITFP: movl 4(%ebp), %eax
+	// IA32-NOOMITFP: popl %ebp
+	// IA32-NOOMITFP: ret
+	// IA32-OMITFP: movl (%esp), %eax
+	// IA32_OMITFP: ret
+	return __builtin_return_address(0);
+}
