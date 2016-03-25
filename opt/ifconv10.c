6dacd65848c7ad02ce53b7dfff61debe49631813
Sebastian Buchwald <Sebastian.Buchwald@kit.edu>
2016-03-25 17:34:30 +0100

    Simplify test.

--- opt/ifconv10.c
+++ opt/ifconv10.c
@@ -1,24 +1,12 @@
 int a;
-int b;
-int c;
-
-int fn1(long long p1)
-{
-	return 0;
-}
-
-void fn2(void) {
-	for (;;) {
-		int d = 0;
-		c && (d = a != 0);
-		if (c)
-			b = fn1(d >= 1);
-		else
-			return;
-	}
-}
+short b;
 
 int main(void)
 {
+	b = !b;
+	while (a) {
+		a = !b;
+	}
+
 	return 0;
 }
