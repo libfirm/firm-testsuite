9a01425ae26a51dbe733d316a8282c93990a9abb
Matthias Braun <matze@braunis.de>
2016-04-04 00:39:18 -0700

    Transform rol_ror.c to lit style and adapt to changed output

--- lit/rol_ror.c
+++ lit/rol_ror.c
@@ -1,13 +1,6 @@
-/*$ check roll\s+_?g $*/
-/*$ check roll\s+\$0x5, _?h $*/
-/*$ check roll\s+\$0x1F, _?i $*/
-/*$ check roll\s+\$0x1B, _?j $*/
-/*$ check rorl\s+%.*, _?k $*/
+// RUN: {cc} -target i686-linux-gnu -integrated-cpp -O3 -S -o - {input} | FileCheck {input}
 
 /* Test wether firm generates destination address mode for rol/ror constructs */
-/* TODO: let's hope the regexs don't break because of different integer
- * representations or using equivalent ror instead of rol for the known size
- * rol/rors */
 
 unsigned g = 0;
 unsigned h = 0;
@@ -16,12 +9,18 @@ unsigned j = 0;
 unsigned k = 0;
 unsigned l = 0;
 
+// CHECK-LABEL: main:
 int main(void)
 {
+	// CHECK: roll g
 	g = (g<<1) | (g>>31);
+	// CHECK: roll $5, h
 	h = (h<<5) | (h>>27);
+	// CHECK: roll $31, i
 	i = (i>>1) | (i<<31);
+	// CHECK: roll $27, j
 	j = (j>>5) | (j<<27);
+	// CHECK: rorl {{.*}}, k
 	k = (k>>l) | (k<<-l);
 	return 0;
 }
