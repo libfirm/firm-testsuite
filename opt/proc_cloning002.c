3215c1b04f63345bdc823bafc174ca957a0a4b37
Sebastian Buchwald <Sebastian.Buchwald@kit.edu>
2016-05-13 12:10:11 +0200

    Slightly simplify test case.

--- opt/proc_cloning002.c
+++ opt/proc_cloning002.c
@@ -1,13 +1,13 @@
-/*$ -fopt-proc-clone -fclone-threshold=0 $*/
-void fn1(int p1)
+/*$ -fopt-proc-clone -fclone-threshold=0 -fno-inline $*/
+int a;
+
+int fn1(int p1)
 {
 	(void)p1;
-	for (;;) {
-		fn1(0);
-	}
+	return a;
 }
 
 int main(void)
 {
-	return 0;
+	return fn1(0);
 }
