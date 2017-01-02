2b6429c47f237cd894ec7b53ae084fedec36e762
Christoph Mallon <christoph.mallon@gmx.de>
2017-01-02 17:38:09 +0100

    Adjust whitespace.
    This makes the reported error locations independent of the usage of an external or the internal preprocessor.

--- C/should_fail/asm.c
+++ C/should_fail/asm.c
@@ -1,8 +1,8 @@
 int main(int argc, char **argv)
 {
 	unsigned x;
-	__asm__(  " /* %[DOESNOTEXIST] */" : [foo] "=r" (x) : "r" (argc));
-	__asm__(  "/* %[DOES"
+	__asm__(" /* %[DOESNOTEXIST] */" : [foo] "=r" (x) : "r" (argc));
+	__asm__("/* %[DOES"
 			  "NOTEXIST] */" : [foo] "=r" (x) : "r" (argc));
 	return x-x;
 }
--- C/should_fail/asm2.c
+++ C/should_fail/asm2.c
@@ -1,6 +1,6 @@
 int main(int argc, char **argv)
 {
 	unsigned x;
-	__asm__(  " /* %[foo] */" : [foo] "=r" (x) : [foo] "0" (x));
+	__asm__(" /* %[foo] */" : [foo] "=r" (x) : [foo] "0" (x));
 	return x-x;
 }
