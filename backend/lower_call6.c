60f9fb2be932ea8a78bed8d3cb8919f7a12f8cc7
Sebastian Buchwald <Sebastian.Buchwald@kit.edu>
2017-01-26 16:22:33 +0100

    Add test for AMD64 ABI

---
+++ backend/lower_call6.c
@@ -0,0 +1,24 @@
+int a;
+struct S0 {
+	int f0;
+	int f1;
+	int f2;
+	int f3;
+	int f4;
+};
+struct S1 {
+	int f5;
+	long f6;
+};
+
+struct S0 fn1(int p1, struct S1 p2, int p3, struct S1 p4)
+{
+	for (;;) {
+		p4.f5 || a;
+	}
+}
+
+int main()
+{
+	return 0;
+}
