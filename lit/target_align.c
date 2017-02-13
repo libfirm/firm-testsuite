b55cb2b53c09c59e2fe4b53f64af771e3dc6fe3a
Matthias Braun <matze@braunis.de>
2017-02-13 00:11:19 -0800

    Add i686-pc-mingw32 to target_align test

--- lit/target_align.c
+++ lit/target_align.c
@@ -1,5 +1,6 @@
 // RUN: {cc} -target i686-linux-gnu {input} -integrated-cpp -S -o - | FileCheck {input} --check CHECK --check IA32_LINUX
 // RUN: {cc} -target i686-apple-darwin {input} -integrated-cpp -S -o - | FileCheck {input} --check CHECK --check IA32_DARWIN
+// RUN: {cc} -target i686-pc-mingw32 {input} -integrated-cpp -S -o - | FileCheck {input} --check CHECK --check IA32_MINGW
 // RUN: {cc} -target x86_64-linux-gnu {input} -integrated-cpp -S -o - | FileCheck {input} --check CHECK --check X86_64
 // RUN: {cc} -target x86_64-apple-darwin {input} -integrated-cpp -S -o - | FileCheck {input} --check CHECK --check X86_64
 // RUN: {cc} -target sparc-leon-linux-gnu {input} -integrated-cpp -S -o - | FileCheck {input} --check CHECK --check OTHERS
@@ -10,6 +11,7 @@ int long_double_align = __alignof__(long double);
 // CHECK-LABEL: long_double_align:
 // IA32_LINUX: .long 0x00000004
 // IA32_DARWIN: .long 0x00000010
+// IA32_MINGW: .long 0x00000004
 // X86_64: .long 0x00000010
 // OTHERS: .long 0x00000008
 
@@ -17,6 +19,7 @@ int long_long_align = __alignof__(long long);
 // CHECK-LABEL: long_long_align:
 // IA32_LINUX: .long 0x00000008
 // IA32_DARWIN: .long 0x00000008
+// IA32_MINGW: .long 0x00000008
 // X86_64: .long 0x00000008
 // OTHERS: .long 0x00000008
 
@@ -27,6 +30,7 @@ int double_in_struct_align = __alignof__(X.x);
 // CHECK-LABEL: double_in_struct_align:
 // IA32_LINUX: .long 0x00000004
 // IA32_DARWIN: .long 0x00000004
+// IA32_MINGW: 0x00000008
 // X86_64: .long 0x00000008
 // OTHERS: .long 0x00000008
 
@@ -37,5 +41,6 @@ int long_long_in_struct_align = __alignof__(Y.y);
 // CHECK-LABEL: long_long_in_struct_align:
 // IA32_LINUX: .long 0x00000004
 // IA32_DARWIN: .long 0x00000004
+// IA32_MINGW: 0x00000008
 // X86_64: .long 0x00000008
 // OTHERS: .long 0x00000008
