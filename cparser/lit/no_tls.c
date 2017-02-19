983ce59ea91729422e2d36bab7a718051dd6603f
Matthias Braun <matze@braunis.de>
2017-02-18 22:04:17 -0800

    Use -target instead -bisa

--- cparser/lit/no_tls.c
+++ cparser/lit/no_tls.c
@@ -1,6 +1,6 @@
-// RUN: not {cc} -fno-PIC -integrated-cpp -bisa=TEMPLATE -S -o- {input} 2>&1 | FileCheck {input}
+// RUN: not {cc} -fno-PIC -integrated-cpp -target TEMPLATE-unknown-elf -S -o- {input} 2>&1 | FileCheck {input}
 // RUN: {cc} -integrated-cpp -target i686-linux-gnu {input} -S -o/dev/null
-// RUN: {cc} -integrated-cpp -fno-PIC -bisa=TEMPLATE --print-ast {input} -o/dev/null
+// RUN: {cc} -integrated-cpp -fno-PIC -target TEMPLATE-unknown-elf --print-ast {input} -o/dev/null
 
 // CHECK: [[@FILE]]:[[@LINE+1]]:14: error: Thread local storage not supported by backend
 __thread int a;
