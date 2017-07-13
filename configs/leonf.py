56e98e4d5c3aa99d08bc937a1fa33ea374e5c95c
Andreas Fried <andreas.fried@kit.edu>
2017-07-13 10:30:17 +0200

    Fix LEON hard-float configuration.

--- configs/leonf.py
+++ configs/leonf.py
@@ -1,10 +1,11 @@
+target_triple = "sparc-leon-linux-gnu"
 def config_leonf(option, opt_str, value, parser):
     config = parser.values
     config.arch_dirs    = ["sparccode"]
-    config.arch_cflags  = "-target sparc-leon-linux-gnu -bspiller=daemel -bregalloc=pref"
+    config.arch_cflags  = "-target sparc-leon-linux-gnu"
     config.arch_ldflags = "-static"
     config.runexe       = "qemu-sparc -r 2.6.40 "
-    config.expect_url   = "https://github.com/libfirm/testresults/raw/master/fail_expectations-"
+    config.expect_url   = "https://github.com/libfirm/testresults/raw/master/fail_expectations-" + target_triple
 
 configurations = {
     "leonf": config_leonf
