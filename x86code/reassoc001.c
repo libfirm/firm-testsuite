1c230884dcf8e7ed19d1fef35f72e02072044d55
Andreas Fried <andreas.fried@kit.edu>
2018-03-06 17:08:03 +0100

    Add a test case where the reassociation increases the operation count.
    The test case is placed in x86code/ because we count "ia32_" assembler
    instructions to check whether the reassociation broke the test.

---
+++ x86code/reassoc001.c
@@ -0,0 +1,17 @@
+/*$ -fverbose-asm -O3 $*/
+/*$ check[10] ia32_ $*/
+
+#include <stdbool.h>
+#include <stdint.h>
+
+bool test(char *p, int x, int y)
+{
+	asm volatile("" : : "r" (p), "r" (x), "r" (y) : "memory");
+	char *a = p + 1 + x;
+	char *b = a + y;
+	return a == b;
+}
+
+int main(void)
+{
+}
