b1f63de7dd515f06632e0bb730b51ebf381d07e7
Christoph Mallon <christoph.mallon@gmx.de>
2018-07-16 12:25:35 +0200

    Add explanation to sched_fac.c.

--- perf/sched_fac.c
+++ perf/sched_fac.c
@@ -2,6 +2,25 @@ unsigned fac(unsigned n)
 {
 	unsigned v = 1;
 	while (n != 0) {
+		/*   _        _
+		 *  / \      / \
+		 * |  Phi  Phi  |
+		 * |   ^  ^ ^   |
+		 * |   | /  |   |
+		 * |   *   -1   |
+		 * |   ^    ^   |
+		 *  \_/      \_/
+		 *
+		 * Only scheduling decision: * or -1 first?
+		 * Two separate arguments: (leading to the same decision)
+		 * - * can be placed early:
+		 *   Its left operand dies, so it does not increase register pressure.
+		 *   Also affinity of left Phi can always be fulfilled, because * is the
+		 *   only user of the Phi.
+		 * - -1 should be placed late:
+		 *   Affinity of right Phi can only be fulfilled if its result and operand
+		 *   do not interfere.  This is only possible if -1 is the last user of the
+		 *   Phi. */
 		v *= n--;
 	}
 	return v;
